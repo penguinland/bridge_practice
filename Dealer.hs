a59d9475b21ae09079275c7d509cdfaaa35b1cde
Alan Davidson <alan.davidson@gmail.com>
2018-05-12 01:51:46 -0400

    Trying to format a dealer program

--- Dealer.hs
+++ Dealer.hs
@@ -8,7 +8,7 @@ module Dealer(
 , Deal
 ) where
 
-import Data.List.Utils(join)
+import Data.List.Utils(join, split, wholeMap, fixedWidth)
 import qualified Data.Map.Strict as Map
 import System.Process(readProcess)
 
@@ -35,6 +35,13 @@ addReq expr (Dealer m l) = Dealer m (expr:l)
 addNewReq :: String -> String -> Dealer -> Dealer
 addNewReq name defn = addReq name . addDefn name defn
 
+toProgram :: Dealer -> String
+toProgram (Dealer defns conds) = join "\n" $
+    ["generate 1000000", "produce 1", ""] ++
+    (Map.foldMapWithKey (\k v -> ["  " ++ k ++ " = " ++ v]) defns)
+    ++ ["", "condition",
+        join " && " conds,
+        "action", "    printall"]
 
 --                                           North  East   South  West
 data Deal = Deal T.Direction T.Vulnerability S.Hand S.Hand S.Hand S.Hand
@@ -47,5 +54,8 @@ instance Showable Deal where
           format h = "    {" ++ toLatex h ++ "}\n"
 
 
-eval :: T.Direction -> T.Vulnerability -> Dealer -> IO (Maybe Deal)
-eval = undefined
+eval :: T.Direction -> T.Vulnerability -> Dealer -> IO (Maybe String)
+eval dir vul deal = let
+    result = readProcess "dealer" ["-s", "0"] (toProgram deal)
+  in
+    result >>= return . Just
