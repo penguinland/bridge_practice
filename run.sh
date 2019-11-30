896e5e64c0e049fca6512253fd60d9844952bb4f
Alan Davidson <alan.davidson@gmail.com>
2019-11-30 11:49:01 -0500

    Friendlier output in run.sh

--- run.sh
+++ run.sh
@@ -5,18 +5,24 @@ set -e
 CWD=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 pushd $CWD
 
+echo ""
+echo "Compiling..."
 stack build
+
+echo ""
+echo "Running..."
 # This next line is platform- and version-agnostic, assuming you've only set
 # things up for a single one of each.
 $CWD/.stack-work/dist/*/*/build/bridge-practice-exe/bridge-practice-exe
 
 mv test.tex /tmp
 cp bridge.tex /tmp
-pushd /tmp
+pushd /tmp > /dev/null
 
+echo ""
+echo "Generating PDFs..."
 xelatex test.tex
 mv test.pdf questions.pdf
-
 xelatex "\let\showsolutions\relax\input{test}"
 mv test.pdf solutions.pdf
 
