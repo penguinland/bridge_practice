aa8628df9bee806114753c3b0f3b289f240cf8b2
Alan Davidson <alan.davidson@gmail.com>
2019-12-01 19:38:36 -0500

    Starting in on 2D-2H auctions, not yet at responder's pass-or-correct

--- src/Topics/StandardModernPrecision/TwoDiamondOpeners.hs
+++ src/Topics/StandardModernPrecision/TwoDiamondOpeners.hs
@@ -26,19 +26,29 @@ noDirectOvercall = do
 
 bestFitSpades :: Action
 bestFitSpades = do
-    minSuitLength T.Spades 4
-    maxSuitLength T.Hearts 3
+    minSuitLength T.Spades   4
+    maxSuitLength T.Hearts   3
     maxSuitLength T.Diamonds 6
-    maxSuitLength T.Clubs 3
+    maxSuitLength T.Clubs    3
 
 
 bestFitClubs :: Action
 bestFitClubs = do
-    maxSuitLength T.Spades 3
-    maxSuitLength T.Hearts 3
+    maxSuitLength T.Spades   3
+    maxSuitLength T.Hearts   3
     maxSuitLength T.Diamonds 6
-    minSuitLength T.Clubs 4
-    forbid $ constrain "s3334" ["shape(", ", 3334)"]  -- bid H instead?
+    minSuitLength T.Clubs    4
+    forbid $ constrain "s3334" ["shape(", ", 3334)"]  -- maybe bid H instead
+
+
+fitHearts :: Action
+fitHearts = do
+    alternatives [ constrain "h_2d_resp" ["shape(", ", 3343 + 3352)"]
+                 , do maxSuitLength T.Spades   4  -- With 5-4 majors, bid 2S
+                      minSuitLength T.Hearts   4
+                      maxSuitLength T.Diamonds 6
+                      maxSuitLength T.Clubs    4
+                 ]
 
 
 open :: Situations
@@ -147,7 +157,7 @@ passSignoff2Spades = let
             (if spadeLength == 4 then "." else
                 ", even though you might be in a 7-card fit.")
       in
-        situation "P2S" dealer vul action (T.Pass) explanation
+        situation "2SP" dealer vul action (T.Pass) explanation
   in
     wrap $ base sit <~ T.allDirections <~ T.allVulnerabilities <~ [3, 4]
 
@@ -188,9 +198,83 @@ passSignoffClubs = let
             "Partner has less-than-invitational values and is signing off. " ++
             "Just pass."
       in
-        situation "P3C" dealer vul action (T.Pass) explanation
+        situation "3CP" dealer vul action (T.Pass) explanation
   in
     wrap $ base sit <~ T.allDirections <~ T.allVulnerabilities
+
+
+immediateSignoffHearts :: Situations
+immediateSignoffHearts = let
+    sit dealer vul = let
+        action = do
+            B.setDealerAndOpener dealer T.North
+            twoDiamondOpener
+            makeCall (T.Bid 2 T.Diamonds)
+            noDirectOvercall
+            fitHearts
+            pointRange 0 9
+        explanation fmt =
+            "Without the strength to invite to game, sign off in " ++
+            output fmt (T.Bid 2 T.Hearts) ++ ". Remember that opener might " ++
+            "pull the bid to " ++ output fmt (T.Bid 2 T.Spades) ++ " with " ++
+            "exactly 4315 shape."
+      in
+        situation "2H" dealer vul action (T.Bid 2 T.Hearts) explanation
+  in
+    wrap $ base sit <~ T.allDirections <~ T.allVulnerabilities
+
+
+passSignoffHearts :: Situations
+passSignoffHearts = let
+    sit dealer vul = let
+        action = do
+            B.setDealerAndOpener dealer T.South
+            twoDiamondOpener
+            makeCall (T.Bid 2 T.Diamonds)
+            noDirectOvercall
+            fitHearts
+            pointRange 0 9
+            makeCall (T.Bid 2 T.Hearts)
+            forbid $ B.takeoutDouble T.Clubs
+            noDirectOvercall
+            minSuitLength T.Hearts 4
+        explanation _ =
+            "Partner has less-than-invitational values and is signing off. " ++
+            "Given that you have 4 hearts, pass."
+      in
+        situation "2HP" dealer vul action (T.Pass) explanation
+  in
+    wrap $ base sit <~ T.allDirections <~ T.allVulnerabilities
+
+
+correctSignoffHearts :: Situations
+correctSignoffHearts = let
+    sit dealer vul heartLength = let
+        action = do
+            B.setDealerAndOpener dealer T.South
+            twoDiamondOpener
+            makeCall (T.Bid 2 T.Diamonds)
+            noDirectOvercall
+            fitHearts
+            maxSuitLength T.Hearts heartLength
+            -- With 7-9 HCP and 5+ hearts, make a mixed raise instead!
+            alternatives [ pointRange 0 9 >> maxSuitLength T.Hearts 4
+                         , pointRange 0 6]
+            makeCall (T.Bid 2 T.Hearts)
+            forbid $ B.takeoutDouble T.Clubs
+            noDirectOvercall
+            maxSuitLength T.Hearts 3
+        explanation fmt =
+            "Partner has less-than-invitational values and is signing off. " ++
+            "With 4315 shape, though, pull this to " ++
+            output fmt (T.Bid 2 T.Spades) ++ " in case partner only had 3 " ++
+            "hearts (e.g., 3343 or 3352 shape)."
+      in
+        situation "2H2S" dealer vul action (T.Pass) explanation
+  in
+    -- We want to commonly show times when responder has just 3 hearts (and
+    -- we're avoiding a 3-3 fit) and times when responder has a real heart suit.
+    wrap $ base sit <~ T.allDirections <~ T.allVulnerabilities <~ [3, 6]
 
 
 topic :: Topic
@@ -203,4 +287,7 @@ topic = Topic "SMP 2D auctions" "SMP2D" situations
                       , passSignoff2Spades
                       , immediateSignoffClubs
                       , passSignoffClubs
+                      , immediateSignoffHearts
+                      , wrap [ passSignoffHearts
+                             , correctSignoffHearts]
                       ]
