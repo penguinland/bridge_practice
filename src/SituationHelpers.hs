cba0983f8a8635275579a17672b2e53405e46f4a
Alan Davidson <alan@privacy.com>
2020-07-22 21:02:29 -0400

    Add the standard wrappers I think will work better

--- src/SituationHelpers.hs
+++ src/SituationHelpers.hs
@@ -1,13 +1,17 @@
 module SituationHelpers (
   anyVulnerability
 , anyVulDlr
+, wrapVulDlr
+, stdWrap
 ) where
+
+import System.Random(StdGen)
 
 import Auction(Action)
 import Output(Commentary)
 import Situation(situation, Situation)
 import Topic(base, (<~), wrap, Situations)
-import Terminology(allDirections, allVulnerabilities, Call)
+import Terminology(Direction, allDirections, Vulnerability, allVulnerabilities, Call)
 
 
 anyVulnerability :: Situation -> Situation
@@ -18,3 +22,15 @@ anyVulDlr debug action call commentary = let
     helper dir vul = situation debug dir vul action call commentary
   in
     wrap $ base helper <~ allDirections <~ allVulnerabilities
+
+
+-- Used for taking a situation that has already had some options applied to it,
+-- and giving it the options of any vulnerability and dealer
+wrapVulDlr :: (StdGen -> Vulnerability -> Direction -> Situation) -> Situations
+wrapVulDlr sit = wrap $ sit <~ allVulnerabilities <~ allDirections
+
+
+-- Used for making Situations where the only parameterization is the
+-- vulnerability and dealer.
+stdWrap :: (Vulnerability -> Direction -> Situation) -> Situations
+stdWrap = wrapVulDlr . base
