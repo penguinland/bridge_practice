e698273a8a6d6569a3555a8fe92497150ff9e2a6
Alan Davidson <alan.davidson@gmail.com>
2021-11-03 23:33:57 -0400

    Rephrase 1D alert for updated rulings

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,11 @@ import System.Random(mkStdGen)
 
 --import qualified Topics.JacobyTransfers as JacobyTransfers
 --import qualified Topics.MinorTransfersScott as MinorTransfers
---import qualified Topics.PrecisionOpeners as PrecisionOpeners
+import qualified Topics.PrecisionOpeners as PrecisionOpeners
 --import qualified Topics.StandardModernPrecision.TwoDiamondOpeners as TwoDOpen
-import qualified Topics.StandardOpeners as StandardOpeners
+--import qualified Topics.StandardOpeners as StandardOpeners
 import ProblemSet(outputLatex)
 
 
 main :: IO ()
-main = outputLatex 100 [StandardOpeners.topic] "test" (mkStdGen 0) >> return ()
+main = outputLatex 100 [PrecisionOpeners.topic] "test" (mkStdGen 0) >> return ()
