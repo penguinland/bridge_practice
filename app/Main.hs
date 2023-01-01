d9f36c04b8f7a6dd808a4aff3b41686d05de47dd
Alan Davidson <alan.davidson@gmail.com>
2022-12-31 22:51:43 -0500

    Start the topic for responses to 1D

--- app/Main.hs
+++ app/Main.hs
@@ -4,22 +4,24 @@ import System.Random(mkStdGen)
 --import qualified Topics.MinorTransfersScott as MinorTransfers
 --import qualified Topics.StandardOpeners as StandardOpeners
 
-import qualified Topics.StandardModernPrecision.OpeningBids as SmpOpenings
-import qualified Topics.StandardModernPrecision.ResponsesToStrongClub as Smp1CResponses
-import qualified Topics.StandardModernPrecision.Mafia as Mafia
-import qualified Topics.StandardModernPrecision.MafiaResponses as MafiaResponses
-import qualified Topics.StandardModernPrecision.TwoDiamondOpeners as TwoDOpen
+--import qualified Topics.StandardModernPrecision.OpeningBids as SmpOpenings
+--import qualified Topics.StandardModernPrecision.ResponsesToStrongClub as Smp1CResponses
+--import qualified Topics.StandardModernPrecision.Mafia as Mafia
+--import qualified Topics.StandardModernPrecision.MafiaResponses as MafiaResponses
+--import qualified Topics.StandardModernPrecision.TwoDiamondOpeners as TwoDOpen
+import qualified Topics.StandardModernPrecision.OneDiamondResponses as OneDResponses
 
 import ProblemSet(outputLatex)
 
 
 main :: IO ()
 main = let
-    topics = [ SmpOpenings.topic
-             , Smp1CResponses.topic
-             , Mafia.topic
-             , MafiaResponses.topic
-             , TwoDOpen.topic
+    topics = [ --SmpOpenings.topic
+             --, Smp1CResponses.topic
+             --, Mafia.topic
+             --, MafiaResponses.topic
+             --, TwoDOpen.topic
+             OneDResponses.topic
              ]
   in do
     outputLatex 100 topics "test" (mkStdGen 0)
---
+++ src/Topics/StandardModernPrecision/OneDiamondResponses.hs
@@ -0,0 +1,40 @@
+module Topics.StandardModernPrecision.OneDiamondResponses(topic) where
+
+--import Output(output)
+import Topic(Topic(..), wrap, Situations)
+import Auction(withholdBid, {-forbid, makePass,-} maxSuitLength, minSuitLength, {-suitLength,-}
+               {-Action, balancedHand,-} pointRange{-, SuitLengthComparator(..), compareSuitLength-})
+import Situation(situation, (<~))
+--import CommonBids(cannotPreempt)
+import qualified Terminology as T
+import Topics.StandardModernPrecision.BasicBids(oppsPass, b1D, smpWrapN)
+import qualified Topics.StandardModernPrecision.Bids1D as B
+
+
+twoMinor6M :: Situations
+twoMinor6M = let
+    sit (minor, bid) major = let
+        action = do
+            b1D
+            oppsPass
+            minSuitLength minor 6
+            minSuitLength major 4
+            maxSuitLength major 5
+            pointRange 14 40
+            withholdBid bid
+        explanation _ =
+            "With game-forcing strength, a 4- or 5-card major, but a 6+ card\
+           \ minor, start by bidding 2 of the minor. There will be time to\
+           \ show the major afterward."
+      in
+        situation "6m" action bid explanation
+  in
+    smpWrapN $ return sit <~ [(T.Clubs, B.b1D2C), (T.Diamonds, B.b1D2D)]
+                          <~ [T.Hearts, T.Spades]
+
+
+topic :: Topic
+topic = Topic "responses to SMP 1D openings" "smp1d" situations
+  where
+    situations = wrap [ twoMinor6M
+                      ]
