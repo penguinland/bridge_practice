87b71fd5ef54fdb2b08f6d457b8c5eb4d743aa67
Alan Davidson <alan.davidson@gmail.com>
2023-01-02 23:32:07 -0500

    update Main.hs to include everything

--- app/Main.hs
+++ app/Main.hs
@@ -4,24 +4,24 @@ import System.Random(mkStdGen)
 --import qualified Topics.MinorTransfersScott as MinorTransfers
 --import qualified Topics.StandardOpeners as StandardOpeners
 
---import qualified Topics.StandardModernPrecision.OpeningBids as SmpOpenings
---import qualified Topics.StandardModernPrecision.ResponsesToStrongClub as Smp1CResponses
---import qualified Topics.StandardModernPrecision.Mafia as Mafia
---import qualified Topics.StandardModernPrecision.MafiaResponses as MafiaResponses
---import qualified Topics.StandardModernPrecision.TwoDiamondOpeners as TwoDOpen
-import qualified Topics.StandardModernPrecision.OneDiamondResponses as OneDResponses
+import qualified Topics.StandardModernPrecision.OpeningBids as SmpOpenings
+import qualified Topics.StandardModernPrecision.ResponsesToStrongClub as Smp1CResponses
+import qualified Topics.StandardModernPrecision.OneDiamondResponses as Smp1DResponses
+import qualified Topics.StandardModernPrecision.Mafia as Mafia
+import qualified Topics.StandardModernPrecision.MafiaResponses as MafiaResponses
+import qualified Topics.StandardModernPrecision.TwoDiamondOpeners as Smp2DOpen
 
 import ProblemSet(outputLatex)
 
 
 main :: IO ()
 main = let
-    topics = [ --SmpOpenings.topic
-             --, Smp1CResponses.topic
-             --, Mafia.topic
-             --, MafiaResponses.topic
-             --, TwoDOpen.topic
-             OneDResponses.topic
+    topics = [ SmpOpenings.topic
+             , Smp1CResponses.topic
+             , Mafia.topic
+             , MafiaResponses.topic
+             , Smp1DResponses.topic
+             , Smp2DOpen.topic
              ]
   in do
     outputLatex 100 topics "test" (mkStdGen 0)
