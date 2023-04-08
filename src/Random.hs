cb0d4f75bdf16fa4516cf4ca57417b7c25b143a7
Alan Davidson <alan.davidson@gmail.com>
2023-04-08 14:10:10 -0400

    tweak: replace Random.use with State.state

--- src/Random.hs
+++ src/Random.hs
@@ -1,22 +1,13 @@
 module Random(
-  use
-, pickItem
+  pickItem
 ) where
 
-import Control.Monad.Trans.State.Strict(State, get, put)
+import Control.Monad.Trans.State.Strict(State, state)
 import System.Random(StdGen, randomR)
-
-
-use :: (StdGen -> (a, StdGen)) -> State StdGen a
-use f = do
-    g <- get
-    let (a, g') = f g
-    put g'
-    return a
 
 
 pickItem :: [a] -> State StdGen a
 pickItem [] = error "Picked item from empty list"
 pickItem as = do
-    i <- use $ randomR (0, length as - 1)
+    i <- state $ randomR (0, length as - 1)
     return (as !! i)
