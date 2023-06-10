5dbb912204e2ce45ceb63791fb71b06539db8573
Alan Davidson <alan.davidson@gmail.com>
2023-06-10 12:05:45 -0400

    standardized names for bidding actions

--- app/Main.hs
+++ app/Main.hs
@@ -2,23 +2,26 @@ import System.Random(mkStdGen)
 
 --import qualified Topics.JacobyTransfers as JacobyTransfers
 --import qualified Topics.MinorTransfersScott as MinorTransfers
---import qualified Topics.StandardOpeners as StandardOpeners
+import qualified Topics.StandardOpeners as StandardOpeners
 
+{-
 import qualified Topics.StandardModernPrecision.OpeningBids as SmpOpenings
 import qualified Topics.StandardModernPrecision.OneClubResponses as Smp1CResponses
 import qualified Topics.StandardModernPrecision.OneDiamondResponses as Smp1DResponses
 import qualified Topics.StandardModernPrecision.Mafia as Mafia
 import qualified Topics.StandardModernPrecision.MafiaResponses as MafiaResponses
 import qualified Topics.StandardModernPrecision.TwoDiamondOpeners as Smp2DOpen
+-}
 
 import ProblemSet(outputLatex)
 
 
 main :: IO ()
 main = let
-    topics = [ --SmpOpenings.topic
+    topics = [ StandardOpeners.topic
+             --SmpOpenings.topic
              --, Smp1CResponses.topic
-              Smp1CResponses.topicExtras
+              --Smp1CResponses.topicExtras
              --, Mafia.topic
              --, MafiaResponses.topic
              --, Smp1DResponses.topic
