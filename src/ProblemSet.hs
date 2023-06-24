5bbe28cd5b24dac92c4906fcfa23a1ffdfaa4297
Alan Davidson <alan.davidson@gmail.com>
2023-06-24 19:11:37 -0400

    the final problem is that the StdGen format is too long

--- src/ProblemSet.hs
+++ src/ProblemSet.hs
@@ -15,8 +15,9 @@ import SituationInstance(instantiate, SituationInstance)
 import Topic(Topic, topicName, refName, choose)
 
 
+-- TODO: find a more compact way to `show g`.
 reference :: String -> String -> StdGen -> String
-reference topic sit g = topic ++ "." ++ sit ++ " " ++ show g
+reference topic sit _ = topic ++ "." ++ sit ++ " "-- ++ show g
 
 
 generate :: Int -> [Topic] -> StdGen -> IO [SituationInstance]
