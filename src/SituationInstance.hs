daa449d6d6aa4e472a0825cb7c9077766e81d763
Alan Davidson <alan.davidson@gmail.com>
2024-06-30 12:42:20 -0400

    implement ToJSON for SituationInstance

--- src/SituationInstance.hs
+++ src/SituationInstance.hs
@@ -5,8 +5,10 @@ module SituationInstance (
 
 
 import Control.Monad.Trans.State.Strict(State, state)
+import Data.Aeson(ToJSON, toJSON)
 import Data.Bifunctor(first)
 import Data.List.Utils(join)
+import Data.Map(fromList)
 import System.Random(StdGen, genWord64)
 
 import DealerProg(eval)
@@ -26,6 +28,15 @@ instance Showable SituationInstance where
             join "%\n}{%\n" [toLatex d, toLatex b, toLatex c, toLatex s, ds] ++
             "%\n}"
     toHtml _ = "todo"
+
+instance ToJSON SituationInstance where
+    toJSON (SituationInstance b c s d ds) = toJSON . fromList $
+        [ ("bidding",      toJSON b)
+        , ("answer",       toJSON . toHtml $ c)
+        , ("explanation",  toJSON . toHtml $ s)
+        , ("deal",         toJSON d)
+        , ("debug_string", toJSON ds)
+        ]
 
 
 -- TODO: Find a way to make this cleaner. Monad transformers might be a relevant
