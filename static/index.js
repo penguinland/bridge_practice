138ec9fb791d839cec514037a0d794febe5a3384
Alan Davidson <alan.davidson@gmail.com>
2024-06-30 22:00:33 -0400

    make the topics checkboxes

--- static/index.js
+++ static/index.js
@@ -14,13 +14,21 @@ async function getTopics() {
 getTopics().then(topics => {
     const topic_section = document.getElementById('topic_list')
     Object.entries(topics).forEach(([index, name]) => {
-        const entry = document.createElement('span');
-        entry.innerHTML = index + ": ";
-        const italics = document.createElement('i');
-        italics.innerHTML = name;
+        const tag_name = "topic_" + index;
+        const checkbox = document.createElement('input');
+        checkbox.type  = "checkbox"
+        checkbox.id    = tag_name;
+        checkbox.name  = tag_name;
+        checkbox.value = name;
+
+        const label = document.createElement('label');
+        label.for       = tag_name;
+        label.innerHTML = name;
+
         newline = document.createElement('br')
-        topic_section.appendChild(entry);
-        topic_section.appendChild(italics);
+
+        topic_section.appendChild(checkbox);
+        topic_section.appendChild(label);
         topic_section.appendChild(newline);
     })
 })
