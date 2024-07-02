a59e86c410196c81873cbde6f183a3b8218bddbd
Alan Davidson <alan.davidson@gmail.com>
2024-07-02 01:19:46 -0400

    More JS: everything except alerts is minimally working (#39)
    It's ugly, but I don't want to put the effort into making it pretty yet. First, I need to figure out footnotes.
    
    
    * refactor to simplify
    
    * Add a table for displaying the problem
    
    * const instead of var
    
    * display bidding in a table; needs footnotes for proper formatting
    
    * Add HTML to format the deal
    
    * populate the dealer and vulnerability
    
    * show the South hand when you get a new problem
    
    * tweaks
    
    * show and hide the other 3 hands
    
    * set the deal to the right height even without showing all hands
    
    * show the solution
    
    * layout tweaks
    
    * more tweaks
    
    * even more tweaks
    
    * fix explanation of 19-20 2N in SMP
    
    * tweak
    
    * tweak: add space
    
    * tweak: indent HTML 2 spaces instead of 4
    
    * undo useless tweak

--- static/index.js
+++ static/index.js
@@ -15,16 +15,13 @@ async function getTopics() {
     return await getJson("/topics");
 }
 
-function findSelectedTopics() {
-    checkboxes = document.getElementsByName("topics");
-    selected = Array.prototype.slice.call(checkboxes).filter(
-        ch => ch.checked == true);
-    return [...selected].map(t => t.value);
-}
-
 async function getSituation() {
-    selected = findSelectedTopics().join(",");
-    return await getJson("/situation/" + selected);
+    const checkboxes = document.getElementsByName("topics");
+    const indices = Array.prototype.slice.call(checkboxes)
+        .filter(ch => ch.checked == true)
+        .map(t => t.value)
+        .join(",");
+    return await getJson("/situation/" + indices);
 }
 
 getTopics().then(topics => {
@@ -54,10 +51,92 @@ getTopics().then(topics => {
     displayProblem();
 })
 
-async function displayProblem () {
-    getSituation().then(sitInst => {
-        sit = document.getElementById("current_situation");
-        sit.innerHTML = JSON.stringify(sitInst);
-        console.log(sitInst);
+function makeBiddingRow(bids, type) {
+    row = document.createElement("tr");
+    bids.forEach(value => {
+        item = document.createElement(type);
+        item.style = "text-align: left; padding-left: 0.5em; padding-right: 0.5em;";
+        item.innerHTML = value;
+        item.width = "25%";
+        row.appendChild(item);
     })
+    return row
+}
+
+function displayBidding(bids) {
+    table = document.createElement("table");
+    table.style = "table-layout:fixed;";
+    table.appendChild(makeBiddingRow(["West", "North", "East", "South"], "th"));
+    bids.forEach(round => {
+        table.appendChild(makeBiddingRow(round, "td"));
+    })
+
+    bidding = clear("bidding");
+    bidding.appendChild(table);
+}
+
+function setValue(id, val) {
+    elem = document.getElementById(id);
+    elem.innerHTML = val;
+}
+
+var current_problem = null;
+
+function clear(id) {
+    elem = document.getElementById(id);
+    while (elem.firstChild) {
+        elem.removeChild(elem.firstChild);
+    }
+    return elem;
+}
+
+async function displayProblem () {
+    getSituation().then(problem => {
+        displayBidding(problem.bidding);
+        setValue("dealer", problem.deal.dealer);
+        setValue("vulnerability", problem.deal.vulnerability);
+        displayHand(problem.deal.south_hand, "south_hand");
+        clear("west_hand");
+        clear("east_hand");
+        clear("north_hand");
+        current_problem = problem;
+
+        show_ans = document.createElement("button");
+        show_ans.innerHTML = "Show Answer";
+        show_ans.onclick = show_answer;
+        expl = clear("explanation");
+        expl.appendChild(show_ans);
+        expl.style = "";
+    })
+}
+
+function displayHand(hand, id) {
+    elem = document.getElementById(id);
+    elem.innerHTML = "&spades;&nbsp;&nbsp;" + hand.spades + "<br/>" +
+                     "<span style='color: red'>&hearts;</span>&nbsp;&nbsp;" +
+                         hand.hearts + "<br/>" +
+                     "<span style='color: red'>&diams;</span>&nbsp;&nbsp;" +
+                         hand.diamonds + "<br/>" +
+                     "&clubs;&nbsp;&nbsp;" + hand.clubs;
+    elem.style = "word-spacing:-0.1em;"
+}
+
+function show_answer() {
+    displayHand(current_problem.deal.west_hand, "west_hand");
+    displayHand(current_problem.deal.east_hand, "east_hand");
+    displayHand(current_problem.deal.north_hand, "north_hand");
+
+    expl = clear("explanation");
+    expl.style="vertical-align:top;";
+    ans = document.createElement("p");
+    ans.innerHTML = "Answer: " + current_problem.answer;
+    expl.appendChild(ans);
+    expl.appendChild(document.createElement("br"));
+    sol = document.createElement("p");
+    sol.innerHTML = current_problem.explanation;
+    expl.appendChild(sol);
+    expl.appendChild(document.createElement("br"));
+    dbg = document.createElement("p")
+    dbg.innerHTML = "debug string: " + current_problem.debug_string;
+    expl.appendChild(dbg);
 }
