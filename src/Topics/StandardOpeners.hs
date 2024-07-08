2ca750e8b8a54975775708ce919077e702514b2a
Alan Davidson <alan.davidson@gmail.com>
2024-07-08 00:28:51 -0400

    tweak: format bid in explanation correctly

--- src/Topics/StandardOpeners.hs
+++ src/Topics/StandardOpeners.hs
@@ -119,8 +119,8 @@ oneDiamond3Cards = let
         "With no 5-card major and a hand unsuitable for opening \
       \ notrump, open " .+ T.Bid 1 T.Diamonds .+ " when \
       \ diamonds is your only minor, even if you only have 3 of them. The \
-      \ only time you'd open 1D with a 3-card suit in standard openings is \
-      \ when your shape is exactly 4=4=3=2."
+      \ only time you'd open " .+ T.Bid 1 T.Diamonds .+ " with a 3-card suit \
+      \ in standard openings is when your shape is exactly 4=4=3=2."
   in
     stdWrap $ situation "1D4432" action SO.b1D explanation
 
