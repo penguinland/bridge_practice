3db4e9b30ea4a37ef41463d9d607e77ae1797c10
Alan Davidson <alan.davidson@gmail.com>
2024-07-14 18:49:16 -0400

    add forcing 1N to the webserver, too

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 import System.Random(mkStdGen)
 
---import qualified Topics.JacobyTransfers as JacobyTransfers
+import qualified Topics.JacobyTransfers as JacobyTransfers
 --import qualified Topics.MinorTransfersScott as MinorTransfers
---import qualified Topics.StandardOpeners as StandardOpeners
---import qualified Topics.TexasTransfers as TexasTransfers
+import qualified Topics.StandardOpeners as StandardOpeners
+import qualified Topics.TexasTransfers as TexasTransfers
 
 {-
 import qualified Topics.StandardModernPrecision.OpeningBids as SmpOpenings
@@ -13,8 +13,8 @@ import qualified Topics.StandardModernPrecision.Mafia as Mafia
 import qualified Topics.StandardModernPrecision.TwoDiamondOpeners as Smp2DOpen
 import qualified Topics.StandardModernPrecision.MafiaResponses as MafiaResponses
 -}
---import qualified Topics.MajorSuitRaises as MajorSuitRaises
---import qualified Topics.Meckwell as Meckwell
+import qualified Topics.MajorSuitRaises as MajorSuitRaises
+import qualified Topics.Meckwell as Meckwell
 import qualified Topics.ForcingOneNotrump as ForcingOneNotrump
 
 import ProblemSet(outputLatex)
@@ -22,11 +22,11 @@ import ProblemSet(outputLatex)
 
 main :: IO ()
 main = let
-    topics = [-- StandardOpeners.topic
-               ForcingOneNotrump.topic
-             --, MajorSuitRaises.topic
-             --, JacobyTransfers.topic
-             --, TexasTransfers.topic
+    topics = [ StandardOpeners.topic
+             , ForcingOneNotrump.topic
+             , MajorSuitRaises.topic
+             , JacobyTransfers.topic
+             , TexasTransfers.topic
              --SmpOpenings.topic
              --, Smp1CResponses.topic
               --Smp1CResponses.topicExtras
@@ -34,7 +34,7 @@ main = let
               --MafiaResponses.topic
              --, Smp1DResponses.topic
              --, Smp2DOpen.topic
-             --, Meckwell.topic
+             , Meckwell.topic
              ]
   in do
     outputLatex 100 topics "test" (mkStdGen 0)
