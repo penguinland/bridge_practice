94f97588c42f5bf61dc00d635b653168bd59c9e0
Alan Davidson <alan.davidson@gmail.com>
2024-07-28 01:44:21 -0400

    make bids for other signoffs over SMP 2D, not used yet

--- src/Bids/StandardModernPrecision/TwoDiamonds.hs
+++ src/Bids/StandardModernPrecision/TwoDiamonds.hs
@@ -3,17 +3,21 @@ module Bids.StandardModernPrecision.TwoDiamonds(
 , b2D2H
 , bP2D2H
 , b2D2H2S
+, b2D2S
+, bP2D2S
+, b2D3C
+, bP2D3C
 ) where
 
 
 import Action(Action)
 import Bids.StandardModernPrecision.BasicBids(b2D, lessThanInvitational)
 import EDSL(suitLength, minSuitLength, maxSuitLength, makeCall,
-            makeAlertableCall, atLeastAsLong)
+            makeAlertableCall, atLeastAsLong, longerThan, forbid)
 import qualified Terminology as T
 
 
--- unexportedhelper function
+-- unexported helper functions
 heartsSignoff :: Action
 heartsSignoff = do
     lessThanInvitational
@@ -21,6 +25,26 @@ heartsSignoff = do
     maxSuitLength T.Diamonds 6
     T.Hearts `atLeastAsLong` T.Spades
     maxSuitLength T.Clubs 4
+
+spadesSignoff :: Action
+spadesSignoff = do
+    lessThanInvitational
+    forbid heartsSignoff
+    minSuitLength T.Spades 3
+    T.Spades `longerThan` T.Hearts
+    maxSuitLength T.Diamonds 6
+    maxSuitLength T.Clubs 4
+
+clubsSignoff :: Action
+clubsSignoff = do
+    lessThanInvitational
+    forbid heartsSignoff
+    forbid spadesSignoff
+    minSuitLength T.Clubs 3
+    maxSuitLength T.Diamonds 6
+    maxSuitLength T.Hearts 2
+    maxSuitLength T.Spades 2
+
 
 b2D2H :: Action
 b2D2H = do
@@ -39,3 +63,29 @@ b2D2H2S :: Action
 b2D2H2S = do
     suitLength T.Hearts 3
     makeAlertableCall (T.Bid 2 T.Spades) "exactly 4315 shape"
+
+
+b2D2S :: Action
+b2D2S = do
+    spadesSignoff
+    makeAlertableCall (T.Bid 2 T.Spades) "nonforcing, likely signoff"
+
+bP2D2S :: Action
+bP2D2S = do
+    spadesSignoff
+    -- When you're a passed hand, bidding a new suit is always nonforcing, so
+    -- that's no longer alertable.
+    makeCall $ T.Bid 2 T.Spades
+
+
+b2D3C :: Action
+b2D3C = do
+    clubsSignoff
+    makeAlertableCall (T.Bid 3 T.Clubs) "nonforcing, likely signoff"
+
+bP2D3C :: Action
+bP2D3C = do
+    clubsSignoff
+    -- When you're a passed hand, bidding a new suit is always nonforcing, so
+    -- that's no longer alertable.
+    makeCall $ T.Bid 3 T.Clubs
