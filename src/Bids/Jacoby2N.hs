4c3f9b00e91d9ea626ca2bdeee95738186044754
Alan Davidson <alan.davidson@gmail.com>
2024-08-16 00:34:00 -0400

    add all the rest of the J2NT rebids by opener

--- src/Bids/Jacoby2N.hs
+++ src/Bids/Jacoby2N.hs
@@ -1,11 +1,28 @@
 module Bids.Jacoby2N(
     b1H  -- Re-exported from StandardOpenings
   , b1H2N
+  , b1H2N3C
+  , b1H2N3D
+  , b1H2N3H
+  , b1H2N3S
+  , b1H2N3N
+  , b1H2N4C
+  , b1H2N4D
+  , b1H2N4H
   , b1H3S
   , b1H4C
   , b1H4D
   , b1S  -- Re-exported from StandardOpenings
   , b1S2N
+  , b1S2N3C
+  , b1S2N3D
+  , b1S2N3H
+  , b1S2N3S
+  , b1S2N3N
+  , b1S2N4C
+  , b1S2N4D
+  , b1S2N4H
+  , b1S2N4S
   , b1S4C
   , b1S4D
   , b1S4H
@@ -63,3 +80,96 @@ b1S2N = do
     pointRange 12 40
     maxLoserCount 7
     makeAlertableCall (T.Bid 2 T.Notrump) "Jacoby: GF with 4+ spades"
+
+
+-- Prefer for opener to rebid a 5-card side suit.
+openerSideSuit_ :: T.Suit -> Action
+openerSideSuit_ sideSuit = do
+    minSuitLength sideSuit 5
+    soundHolding sideSuit  -- Don't do this with a bad 5-card suit
+    makeCall $ T.Bid 4 sideSuit
+
+b1H2N4C :: Action
+b1H2N4C = openerSideSuit_ T.Clubs
+
+b1H2N4D :: Action
+b1H2N4D = openerSideSuit_ T.Diamonds
+
+-- If opener is 5-5 in the majors and opened a heart, they must either have
+-- longer hearts or were planning to reverse.
+-- TODO: what should we do about that? Is there ever a time when you "reverse"
+-- to 4S?
+
+b1S2N4C :: Action
+b1S2N4C = openerSideSuit_ T.Clubs
+
+b1S2N4D :: Action
+b1S2N4D = openerSideSuit_ T.Diamonds
+
+b1S2N4H :: Action
+b1S2N4H = openerSideSuit_ T.Hearts
+
+
+-- Show shortness if you can't show length.
+openerShortness_ :: T.Suit -> [Action] -> Action
+openerShortness_ shortSuit preferredBids = do
+    forbidAll preferredBids
+    maxSuitLength shortSuit 1
+    makeAlertableCall (T.Bid 3 shortSuit) ("shortness in " .+ show shortSuit)
+
+b1H2N3C :: Action
+b1H2N3C = openerShortness_ T.Clubs [b1H2N4C, b1H2N4D]
+
+b1H2N3D :: Action
+b1H2N3D = openerShortness_ T.Diamonds [b1H2N4C, b1H2N4D]
+
+b1H2N3S :: Action
+b1H2N3S = openerShortness_ T.Spades [b1H2N4C, b1H2N4D]
+
+b1S2N3C :: Action
+b1S2N3C = openerShortness_ T.Clubs [b1S2N4C, b1S2N4D, b1S2N4H]
+
+b1S2N3D :: Action
+b1S2N3D = openerShortness_ T.Diamonds [b1S2N4C, b1S2N4D, b1S2N4H]
+
+b1S2N3H :: Action
+b1S2N3H = openerShortness_ T.Hearts [b1S2N4C, b1S2N4D, b1S2N4H]
+
+
+-- If you can't show length or shortness, show strength
+b1H2N4H :: Action
+b1H2N4H = do
+    forbidAll [b1H2N4C, b1H2N4D, b1H2N3C, b1H2N3D, b1H2N3S]
+    pointRange 0 13
+    makeCall $ T.Bid 4 T.Hearts
+
+b1H2N3N :: Action
+b1H2N3N = do
+    forbidAll [b1H2N4C, b1H2N4D, b1H2N3C, b1H2N3D, b1H2N3S]
+    pointRange 14 15
+    makeCall $ T.Bid 3 T.Notrump
+
+b1H2N3H :: Action
+b1H2N3H = do
+    forbidAll [b1H2N4C, b1H2N4D, b1H2N3C, b1H2N3D, b1H2N3S]
+    pointRange 16 40
+    makeCall $ T.Bid 3 T.Hearts
+
+
+b1S2N4S :: Action
+b1S2N4S = do
+    forbidAll [b1S2N4C, b1S2N4D, b1S2N4H, b1S2N3C, b1S2N3D, b1S2N3H]
+    pointRange 0 13
+    makeCall $ T.Bid 4 T.Spades
+
+b1S2N3N :: Action
+b1S2N3N = do
+    forbidAll [b1S2N4C, b1S2N4D, b1S2N4H, b1S2N3C, b1S2N3D, b1S2N3H]
+    pointRange 14 15
+    makeCall $ T.Bid 3 T.Notrump
+
+b1S2N3S :: Action
+b1S2N3S = do
+    forbidAll [b1S2N4C, b1S2N4D, b1S2N4H, b1S2N3C, b1S2N3D, b1S2N3H]
+    pointRange 16 40
+    makeCall $ T.Bid 3 T.Spades
