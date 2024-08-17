0b94caabb162623a4d7a6ec5e65f14b5f682215c
Alan Davidson <alan.davidson@gmail.com>
2024-08-17 18:09:38 -0400

    add situations about opener rebidding shortness

--- src/Topics/Jacoby2NT.hs
+++ src/Topics/Jacoby2NT.hs
@@ -5,7 +5,7 @@ import CommonBids(setOpener, noInterference)
 import Output((.+))
 import Situation(situation, (<~))
 import qualified Terminology as T
-import Topic(wrap, wrapVulNW, Situations, Topic, makeTopic)
+import Topic(wrap, wrapVulNW, wrapVulSE, Situations, Topic, makeTopic)
 
 
 j2nt :: Situations
@@ -25,12 +25,42 @@ j2nt = let
         situation "j2n" action j2ntBid explanation
   in
     -- We must be an unpassed hand to be game-forcing.
-    wrapVulNW $ return sit <~ [(B.b1H, B.b1H2N, T.Hearts),
-                               (B.b1S, B.b1S2N, T.Spades)]
+    wrapVulNW $ return sit <~ [ (B.b1H, B.b1H2N, T.Hearts)
+                              , (B.b1S, B.b1S2N, T.Spades) ]
+
+
+singleton :: Situations
+singleton = let
+    sit (openerBid, j2ntBid, singletonBid, suit) = let
+        action = do
+            setOpener T.South
+            _ <- openerBid
+            noInterference suit
+            _ <- j2ntBid
+            noInterference suit
+        explanation =
+            "Partner has bid Jacoby " .+ T.Bid 2 T.Notrump .+ ". We don't " .+
+            "have a good 5-card side suit to jump into, but we do have " .+
+            "shortness to bid at the cheapest level. This will help " .+
+            "partner decide whether to sign off in " .+ T.Bid 4 suit .+
+            ", or start control bidding to investigate slam. If you play " .+
+            "Serious or Frivolous " .+ T.Bid 3 T.Notrump .+ ", they might " .+
+            "also bid that."
+      in
+        situation "sing" action singletonBid explanation
+  in
+    -- Partner must be an unpassed hand to be game-forcing.
+    wrapVulSE $ return sit <~ [ (B.b1H, B.b1H2N, B.b1H2N3C, T.Hearts)
+                              , (B.b1H, B.b1H2N, B.b1H2N3D, T.Hearts)
+                              , (B.b1H, B.b1H2N, B.b1H2N3S, T.Hearts)
+                              , (B.b1S, B.b1S2N, B.b1S2N3C, T.Spades)
+                              , (B.b1S, B.b1S2N, B.b1S2N3D, T.Spades)
+                              , (B.b1S, B.b1S2N, B.b1S2N3H, T.Spades)
+                              ]
 
 
 topic :: Topic
 topic = makeTopic ("Jacoby ".+ T.Bid 2 T.Notrump)  "J2NT" $
     wrap [ j2nt
-         , j2nt
+         , singleton
          ]
