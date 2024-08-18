4adc985d82b1f63da6104b85d4853275b5213c2d
Alan Davidson <alan.davidson@gmail.com>
2024-08-18 17:53:30 -0400

    tweak point ranges some more

--- src/Bids/Jacoby2NT.hs
+++ src/Bids/Jacoby2NT.hs
@@ -51,8 +51,8 @@ module Bids.Jacoby2NT(
 import Action(Action)
 import Bids.StandardOpenings(b1H, b1S)
 import EDSL(minSuitLength, maxSuitLength, makeCall, makeAlertableCall,
-            pointRange, soundHolding, minLoserCount, maxLoserCount, forbidAll,
-            shorterThan, atMostAsLong, forEach, hasControl, forbid)
+            pointRange, soundHolding, maxLoserCount, forbid, forbidAll,
+            shorterThan, atMostAsLong, forEach, hasControl)
 import Output((.+))
 import qualified Terminology as T
 
@@ -212,42 +212,42 @@ shapelyRebidsS_ = [b1S2N4C, b1S2N4D, b1S2N4H, b1S2N3C, b1S2N3D, b1S2N3H]
 b1H2N4H :: Action
 b1H2N4H = do
     forbidAll shapelyRebidsH_
-    pointRange 0 13
+    pointRange 0 14
     makeCall $ T.Bid 4 T.Hearts
 
 
 b1H2N3N :: Action
 b1H2N3N = do
     forbidAll shapelyRebidsH_
-    pointRange 14 15
+    pointRange 15 16
     makeCall $ T.Bid 3 T.Notrump
 
 
 b1H2N3H :: Action
 b1H2N3H = do
     forbidAll shapelyRebidsH_
-    pointRange 16 40
+    pointRange 17 40
     makeCall $ T.Bid 3 T.Hearts
 
 
 b1S2N4S :: Action
 b1S2N4S = do
     forbidAll shapelyRebidsS_
-    pointRange 0 13
+    pointRange 0 14
     makeCall $ T.Bid 4 T.Spades
 
 
 b1S2N3N :: Action
 b1S2N3N = do
     forbidAll shapelyRebidsS_
-    pointRange 14 15
+    pointRange 15 16
     makeCall $ T.Bid 3 T.Notrump
 
 
 b1S2N3S :: Action
 b1S2N3S = do
     forbidAll shapelyRebidsS_
-    pointRange 16 40
+    pointRange 17 40
     makeCall $ T.Bid 3 T.Spades
 
 
@@ -255,25 +255,25 @@ b1S2N3S = do
 
 b1H2N4HP :: Action
 b1H2N4HP = do
-    minLoserCount 6
+    pointRange 0 16
     makeCall T.Pass
 
 
 b1S2N4SP :: Action
 b1S2N4SP = do
-    minLoserCount 6
+    pointRange 0 16
     makeCall T.Pass
 
 
 b1H2N3N4H :: Action
 b1H2N3N4H = do
-    minLoserCount 7
+    pointRange 0 14
     makeCall $ T.Bid 4 T.Hearts
 
 
 b1S2N3N4S :: Action
 b1S2N3N4S = do
-    minLoserCount 7
+    pointRange 0 14
     makeCall $ T.Bid 4 T.Spades
 
 -- Slam investigations
