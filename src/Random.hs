39f14c300a2a382f51cc913ed23956f3b43ee2ab
Alan Davidson <alan.davidson@gmail.com>
2024-08-19 23:29:10 -0400

    undo changes to pickItem, in anticipation of using mapStateT (return . runIdentity)

--- src/Random.hs
+++ src/Random.hs
@@ -2,11 +2,11 @@ module Random(
   pickItem
 ) where
 
-import Control.Monad.Trans.State.Strict(StateT, state)
+import Control.Monad.Trans.State.Strict(State, state)
 import System.Random(StdGen, randomR)
 
 
-pickItem :: Monad m => [a] -> StateT StdGen m a
+pickItem :: [a] -> State StdGen a
 pickItem [] = error "Picked item from empty list"
 pickItem as = do
     i <- state $ randomR (0, length as - 1)
