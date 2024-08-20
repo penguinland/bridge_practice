275a8be00746c4039313811bf2fe999f8bd05e89
Alan Davidson <alan.davidson@gmail.com>
2024-08-19 23:51:13 -0400

    types all work in ProblemSet, server doesn't compile yet

--- src/ProblemSet.hs
+++ src/ProblemSet.hs
@@ -5,7 +5,8 @@ module ProblemSet(
 
 import Control.Monad(when)
 import Control.Monad.Trans.Class(lift)
-import Control.Monad.Trans.State.Strict(runState, get, put, StateT, State)
+import Control.Monad.Trans.State.Strict(get, StateT, State, mapStateT)
+import Data.Functor.Identity(runIdentity)
 import Data.List.Utils(join, replace)
 import System.IO(hFlush, stdout)
 import System.Random(StdGen)
@@ -22,32 +23,29 @@ reference :: String -> String -> StdGen -> String
 reference topic sit g = topic ++ "." ++ sit ++ " " ++ show g
 
 
-generate :: Int -> [Topic] -> StdGen -> IO ([SituationInstance], StdGen)
-generate 0 _      g = return ([], g)
-generate n topics g = let
-    makeInst :: State StdGen (IO (Maybe SituationInstance))
-    makeInst = do
-        topic <- pickItem topics
-        gen <- get
-        situation <- choose topic
-        let ref = reference (refName topic) (sitRef situation) gen
-        instantiate ref situation
-    (sitInst, g') = runState makeInst g
-  in do
-    when (n `mod` 10 == 0) (putStr "." >> hFlush stdout)
-    maybeSit <- sitInst
+addStack :: Monad m => State StdGen a -> StateT StdGen m a
+addStack = mapStateT (return . runIdentity)
+
+
+generate :: Int -> [Topic] -> StateT StdGen IO [SituationInstance]
+generate 0 _      = return []
+generate n topics = do
+    topic <- addStack $ pickItem topics
+    gen <- get
+    situation <- addStack $ choose topic
+    let ref = reference (refName topic) (sitRef situation) gen
+    maybeSit <- instantiate ref situation
+    when (n `mod` 10 == 0) (lift $ putStr "." >> hFlush stdout)
     case maybeSit of
-        Nothing -> generate n topics g'  -- Try again
+        Nothing -> generate n topics  -- Try again
         Just d  -> do
-            (rest, g'') <- generate (n - 1) topics g'
-            return (d:rest, g'')
+            rest <- generate (n - 1) topics
+            return $ d:rest
 
 
 outputLatex :: Int -> [Topic] -> String -> StateT StdGen IO String
 outputLatex numHands topics filename = do
-    g <- get
-    (problems, g') <- lift $ generate numHands topics g
-    put g'
+    problems <- generate numHands topics
     let topicNames = join ", " . map (toLatex . topicName) $ topics
         problemSet = join "\n" . map toLatex $ problems
     template <- lift $ readFile "template.tex"
