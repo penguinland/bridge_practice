1907384df1cf585fa0f955cf50ffb9e6d32b3f9a
Alan Davidson <alan.davidson@gmail.com>
2024-08-25 01:15:32 -0400

    taking a first stab at a very awkward situation

---
+++ src/Topics/StandardModernPrecision/TripleFourOne.hs
@@ -0,0 +1,48 @@
+module Topics.StandardModernPrecision.TripleFourOne(topic) where
+
+import Bids.StandardModernPrecision.BasicBids(setOpener, oppsPass)
+import Bids.StandardModernPrecision.TwoDiamonds(name44Rkc)
+import qualified Bids.StandardModernPrecision.OneClub as B
+import EDSL(maxSuitLength, pointRange, forEach)
+import Output(Punct(..), (.+))
+import Situation(situation, (<~))
+import qualified Terminology as T
+import Topic(Topic, wrap, wrapVulNW, wrapVulSE, Situations, makeTopic)
+
+
+showAny4441 :: Situations
+showAny4441 = let
+    sit (action, answer, opener, dealers) vul = let
+        explanation =
+            "Bidding " .+ T.Bid 2 T.Spades .+ " shows your 4441 shape. " .+
+            "partner will relay to " .+ T.Bid 2 T.Notrump .+ ", prompting " .+
+            "you to bid your singleton, after which " .+ name44Rkc .+ "is on."
+        inner dealer = let
+            action' = do
+                setOpener opener
+                action
+          in
+            wrap situation "any" action' bid explanation vul dealer
+      in
+        wrap inner <~ dealers
+  in
+    wrap $ return sit <~ [ (do B.b1C
+                               oppsPass
+                               B.b1C1H
+                               oppsPass
+                           , B.b1C1H2S, T.South, [T.South, T.East])
+                         , (do B.b1C
+                               oppsPass
+                           , B.b1C2S, T.North, [T.North, T.West])
+                         , (do B.b1C
+                               oppsPass
+                           , B.bP1C2S, T.North, [T.South, T.East])
+                      <~ T.allVulnerabilities
+
+
+topic :: Topic
+topic = makeTopic description "SMP4441" situations
+  where
+    description = "SMP 4441 hands"
+    situations = wrap [ showAny4441
+                      ]
