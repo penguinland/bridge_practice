56c1a63b25c13f52b0bb595b4743b26a7b8cfb55
Alan Davidson <alan.davidson@gmail.com>
2024-09-16 12:35:09 -0400

    start of Lebensohl over notrump interference (#94)
    Oh boy, this is going to be a big one. Let's start small with this PR, and build up from there. Github Actions caching works across PRs!
    
    
    * start of lebensohl over 1N interference
    
    * add it to the list of supported topics
    
    * situations for signing off at the 2 level
    
    * add TODO for other situations
    
    * add comments

---
+++ src/Bids/Lebensohl.hs
@@ -0,0 +1,76 @@
+module Bids.Lebensohl(
+    b1N  -- re-exported from StandardOpenings
+    -- Systems on over X
+  , b1NoX2C
+  , b1NoX2D
+  , b1NoX2H
+    -- Systems on over 2C
+  , b1No2CX
+  , b1No2C2D
+  , b1No2C2H
+   -- Actual lebensohl bids
+  , b1No2D2H
+  , b1No2D2S
+  , b1No2H2S
+) where
+
+
+import Action(Action, withholdBid)
+import qualified Bids.OneNotrump as NT
+import EDSL(minSuitLength, makeCall, longerThan, pointRange)
+import qualified Terminology as T
+
+
+b1N :: Action
+b1N = NT.b1N
+
+
+-- Ignore the opponents' interference if it doesn't actually take up any bidding
+-- space.
+b1NoX2C :: Action
+b1NoX2C = NT.b1N2C
+
+b1NoX2D :: Action
+b1NoX2D = NT.b1N2D
+
+b1NoX2H :: Action
+b1NoX2H = NT.b1N2H
+
+b1No2CX :: Action
+b1No2CX = do
+    withholdBid NT.b1N2C
+    makeCall T.Double
+
+b1No2C2D :: Action
+b1No2C2D = NT.b1N2D
+
+b1No2C2H :: Action
+b1No2C2H = NT.b1N2H
+
+-- Signoffs
+b1No2D2H :: Action
+b1No2D2H = do
+    NT.lessThanInvitational
+    pointRange 5 40
+    minSuitLength T.Hearts 5
+    -- If you're 5-5 in the majors, pick the better suit. I'm too lazy to add
+    -- something to the EDSL to figure out which suit is better, so avoid it.
+    T.Hearts `longerThan` T.Clubs
+    T.Hearts `longerThan` T.Diamonds
+    T.Hearts `longerThan` T.Spades
+    makeCall $ T.Bid 2 T.Hearts
+
+
+b1No2D2S :: Action
+b1No2D2S = do
+    NT.lessThanInvitational
+    pointRange 5 40
+    minSuitLength T.Spades 5
+    T.Spades `longerThan` T.Clubs
+    T.Spades `longerThan` T.Diamonds
+    T.Spades `longerThan` T.Hearts
+    makeCall $ T.Bid 2 T.Spades
+
+
+b1No2H2S :: Action
+b1No2H2S = b1No2D2S
---
+++ src/Topics/Lebensohl.hs
@@ -0,0 +1,104 @@
+module Topics.Lebensohl(topic) where
+
+import qualified Bids.Lebensohl as B
+import qualified Bids.Meckwell as MW
+import qualified Bids.Cappelletti as Capp
+import qualified Bids.DONT as DONT
+import CommonBids(setOpener)
+--import EDSL(makePass, pointRange, suitLength, maxSuitLength, forEach)
+import Output((.+))
+import Situation(situation, (<~))
+import qualified Terminology as T
+import Topic(Topic, wrap, Situations, makeTopic)
+
+
+ignoreOpps :: Situations
+ignoreOpps = let
+    sit (overcall, response, overcallIs2C) = let
+        action = do
+            setOpener T.North
+            B.b1N
+            overcall
+        explanation =
+            "Partner opened a strong " .+ B.b1N .+ ", and RHO interfered " .+
+            "with the auction. However, their call didn't actually take up " .+
+            "any of our bidding room. Ignore it, and use our usual systems " .+
+            "over notrump" .+
+            (if overcallIs2C then " (double of " .+ overcall .+ " is Stayman)"
+                            else mempty) .+ "."
+      in situation "ignr" action response explanation
+  in
+    -- East should be an unpassed hand to interfere over North's notrump.
+    wrap $ return sit <~ [ (DONT.b1NoX,  B.b1NoX2C,  False)
+                         , (DONT.b1NoX,  B.b1NoX2D,  False)
+                         , (DONT.b1NoX,  B.b1NoX2H,  False)
+                         , (MW.b1NoX,    B.b1NoX2C,  False)
+                         , (MW.b1NoX,    B.b1NoX2D,  False)
+                         , (MW.b1NoX,    B.b1NoX2H,  False)
+                         , (Capp.b1NoX,  B.b1NoX2C,  False)
+                         , (Capp.b1NoX,  B.b1NoX2D,  False)
+                         , (Capp.b1NoX,  B.b1NoX2H,  False)
+                         , (DONT.b1No2C, B.b1No2CX,  True)
+                         , (DONT.b1No2C, B.b1No2C2D, True)
+                         , (DONT.b1No2C, B.b1No2C2H, True)
+                         , (MW.b1No2C,   B.b1No2CX,  True)
+                         , (MW.b1No2C,   B.b1No2C2D, True)
+                         , (MW.b1No2C,   B.b1No2C2H, True)
+                         , (Capp.b1No2C, B.b1No2CX,  True)
+                         , (Capp.b1No2C, B.b1No2C2D, True)
+                         , (Capp.b1No2C, B.b1No2C2H, True)
+                         ]
+                      <~ T.allVulnerabilities
+                      <~ [T.North, T.South, T.West]
+
+
+signoff :: Situations
+signoff = let
+    sit (overcall, response) = let
+        action = do
+            setOpener T.North
+            B.b1N
+            overcall
+        explanation =
+            "Partner opened a strong " .+ B.b1N .+ ", and RHO interfered " .+
+            "with the auction. We're so weak we don't even want to invite " .+
+            "to game, but we do have enough strength to suspect this is " .+
+            "our contract. Bid our suit at the 2 level, as signoff."
+      in situation "snof" action response explanation
+  in
+    wrap $ return sit <~ [ (DONT.b1No2D, B.b1No2D2H)
+                         , (DONT.b1No2D, B.b1No2D2S)
+                         -- If RHO shows both majors, don't bid a major!
+                         --, (DONT.b1No2H, B.b1No2H2S)
+                         , (MW.b1No2D,   B.b1No2D2H)
+                         , (MW.b1No2D,   B.b1No2D2S)
+                         , (MW.b1No2H,   B.b1No2H2S)
+                         -- If RHO shows both majors, don't bid a major!
+                         --, (Capp.b1No2D, B.b1No2D2H)
+                         --, (Capp.b1No2D, B.b1No2D2S)
+                         , (Capp.b1No2H, B.b1No2H2S)
+                         ]
+                      <~ T.allVulnerabilities
+                      -- East should be an unpassed hand to interfere.
+                      <~ [T.North, T.South, T.West]
+
+
+-- TODO:
+-- natural GF bids at the 3 level
+-- jump to 3N
+-- relay to 3N (answer should be 2N planning to rebid 3N)
+-- cue bid for Stayman
+-- relay to cue bid (answer should be 2N planning to rebid the cue)
+-- complete the relay
+-- Texas transfers over interference (in the Texas Transfers topic)
+-- make the opponents sometimes use natural overcalls
+
+
+topic :: Topic
+topic = makeTopic
+    ("lebensohl after interference over our " .+ T.Bid 1 T.Notrump)
+    "leb1N" situations
+  where
+    situations = wrap [ ignoreOpps
+                      , signoff
+                      ]
