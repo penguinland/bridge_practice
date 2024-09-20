db3445f33632a37ce8b98010b70eb4e51eb346f3
Alan Davidson <alan.davidson@gmail.com>
2024-09-20 15:48:15 -0400

    Reorganize preempts over notrump (#99)
    Put them in with the natural bids, and then you can re-export them from other places. Also, put the minimum points needed into `singleSuited` and `twoSuited`, instead of each and every place those get used. Thirdly, I'm forbidding pre-empts in single- and two-suited hands, though that might need tweaking later.
    
    
    * add the pre-empts to the natural defense
    
    * ...and remove them from DONT
    
    * make singleSuited and twoSuited have the strength requirements
    
    * fix build

--- src/Bids/DONT.hs
+++ src/Bids/DONT.hs
@@ -8,7 +8,7 @@ module Bids.DONT(
   , b1No2D2H
   , b1No2H
   , b1No2S
-  , b1No3C
+  , b1No3C  -- These next four are all re-exported from NaturalOneNotrumpDefense
   , b1No3D
   , b1No3H
   , b1No3S
@@ -17,28 +17,15 @@ module Bids.DONT(
 
 import Action(Action)
 import Bids.StandardOpenings(b1N)
-import Bids.NaturalOneNotrumpDefense(singleSuited, twoSuited)
+import Bids.NaturalOneNotrumpDefense(
+    singleSuited, twoSuited, b1No3C, b1No3D, b1No3H, b1No3S, minPointsToCompete)
 import EDSL(pointRange, alternatives, makeCall, makeAlertableCall, forEach,
-            longerThan, minSuitLength, maxSuitLength, soundHolding, forbid,
-            forbidAll)
+            longerThan, minSuitLength, forbid)
 import qualified Terminology as T
-
-
-
--- What's the right minimum strength to bid DONT? It kinda depends on the
--- vulnerability and where in the hand this strength is located. Let's guess 10
--- is a pretty decent minimum, but I'm open to changing it later.
-minPointsToCompete :: Int
-minPointsToCompete = 10
-
-pointsToCompete :: Action
-pointsToCompete = pointRange minPointsToCompete 40
 
 
 b1NoX :: Action
 b1NoX = do
-    shouldntPreempt
-    pointsToCompete
     -- If you're single-suited with spades, bid 2S with a minimum, and
     -- double-then-bid with extras.
     forbid b1No2S
@@ -52,7 +39,6 @@ b1NoX2C = makeAlertableCall (T.Bid 2 T.Clubs) "pass or correct"
 
 b1No2C :: Action
 b1No2C = do
-    pointsToCompete
     alternatives $ map (twoSuited T.Clubs) [T.Diamonds, T.Hearts, T.Spades]
     makeAlertableCall (T.Bid 2 T.Clubs) "clubs and another suit"
 
@@ -70,7 +56,6 @@ b1No2C2D = do
 
 b1No2D :: Action
 b1No2D = do
-    pointsToCompete
     alternatives $ map (twoSuited T.Diamonds) T.majorSuits
     makeAlertableCall (T.Bid 2 T.Diamonds) "diamonds and a major"
 
@@ -88,41 +73,13 @@ b1No2D2H = do
 
 b1No2H :: Action
 b1No2H = do
-    pointsToCompete
     twoSuited T.Hearts T.Spades
     makeAlertableCall (T.Bid 2 T.Hearts) "both majors"
 
 
 b1No2S :: Action
 b1No2S = do
-    pointsToCompete
-    shouldntPreempt
     -- If you've got extra strength, double and then bid 2S afterward.
     pointRange 0 (minPointsToCompete + 2)
     singleSuited T.Spades
     makeCall $ T.Bid 2 T.Spades
-
-
-preempt_ :: T.Suit -> Action
-preempt_ suit = do
-    pointsToCompete
-    minSuitLength suit 7
-    soundHolding suit  -- Don't do this with a bad suit, even if you're nonvul.
-    -- Some people are reluctant to pre-empt if they've got a side 4-card major.
-    forEach (filter (/= suit) T.majorSuits) (`maxSuitLength` 3)
-    makeCall $ T.Bid 3 suit
-
-b1No3C :: Action
-b1No3C = preempt_ T.Clubs
-
-b1No3D :: Action
-b1No3D = preempt_ T.Diamonds
-
-b1No3H :: Action
-b1No3H = preempt_ T.Hearts
-
-b1No3S :: Action
-b1No3S = preempt_ T.Spades
-
-shouldntPreempt :: Action
-shouldntPreempt = forbidAll[b1No3C, b1No3D, b1No3H, b1No3S]
--- src/Bids/NaturalOneNotrumpDefense.hs
+++ src/Bids/NaturalOneNotrumpDefense.hs
@@ -3,7 +3,13 @@ module Bids.NaturalOneNotrumpDefense(
   , b1No2D
   , b1No2H
   , b1No2S
+  , b1No3C
+  , b1No3D
+  , b1No3H
+  , b1No3S
   -- Export these helpers for other defenses against 1N, too
+  , minPointsToCompete
+  , pointsToCompete
   , singleSuited
   , twoSuited
 ) where
@@ -11,20 +17,25 @@ module Bids.NaturalOneNotrumpDefense(
 
 import Action(Action)
 import EDSL(pointRange, minSuitLength, maxSuitLength, makeCall, alternatives,
-            soundHolding, forEach)
+            soundHolding, forEach, forbidAll)
 import qualified Terminology as T
 
 
 -- What's the right minimum strength to overcall? It kinda depends on the
 -- vulnerability and where in the hand this strength is located. Let's guess 10
 -- is a pretty decent minimum, but I'm open to changing it later.
+minPointsToCompete :: Int
+minPointsToCompete = 10
+
 pointsToCompete :: Action
-pointsToCompete = pointRange 10 40
+pointsToCompete = pointRange minPointsToCompete 40
 
 
 singleSuited :: T.Suit -> Action
 singleSuited suit = do
+    pointsToCompete
     minSuitLength suit 6
+    shouldntPreempt
     soundHolding suit
     forEach (filter (/= suit) T.allSuits) (`maxSuitLength` 3)
 
@@ -37,6 +48,7 @@ singleSuited suit = do
 -- need opinions from someone who has better fundamentals on this stuff.
 twoSuited :: T.Suit -> T.Suit -> Action
 twoSuited a b = do
+    pointsToCompete
     -- With a 6-card suit, you might be better off treating your hand as
     -- single-suited, depending on the suit quality. Rather than program all
     -- that nuance in, we limit ourselves to either 5-4 or 5-5 shapes.
@@ -72,3 +84,29 @@ b1No2H = naturalSingleSuit_ T.Hearts
 
 b1No2S :: Action
 b1No2S = naturalSingleSuit_ T.Spades
+
+
+preempt_ :: T.Suit -> Action
+preempt_ suit = do
+    pointsToCompete
+    -- TODO: Would you still bid at the 4 level with an 8-card suit?
+    minSuitLength suit 7
+    soundHolding suit  -- Don't do this with a bad suit, even if you're nonvul.
+    -- Some people are reluctant to pre-empt if they've got a side 4-card major.
+    forEach (filter (/= suit) T.majorSuits) (`maxSuitLength` 3)
+    makeCall $ T.Bid 3 suit
+
+b1No3C :: Action
+b1No3C = preempt_ T.Clubs
+
+b1No3D :: Action
+b1No3D = preempt_ T.Diamonds
+
+b1No3H :: Action
+b1No3H = preempt_ T.Hearts
+
+b1No3S :: Action
+b1No3S = preempt_ T.Spades
+
+shouldntPreempt :: Action
+shouldntPreempt = forbidAll[b1No3C, b1No3D, b1No3H, b1No3S]
