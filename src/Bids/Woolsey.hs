c17ff461f88061f56981092ed72916aaf51eb51f
Alan Davidson <alan.davidson@gmail.com>
2025-06-15 20:14:59 -0400

    make some of the follow-up bids

--- src/Bids/Woolsey.hs
+++ src/Bids/Woolsey.hs
@@ -8,12 +8,12 @@ module Bids.Woolsey(
 --  , b1NoX2D2H
 --  , b1NoX2D2S
   , b1No2C
-  --, b1No2C2H
-  --, b1No2C2S
+  , b1No2C2H
+  , b1No2C2S
   , b1No2D
-  --, b1No2D2H
-  --, b1No2D2HP
-  --, b1No2D2H2S
+  , b1No2D2H
+  , b1No2D2HP
+  , b1No2D2H2S
   , b1No2H
   --, b1No2H2N
   --, b1No2H2N3C
@@ -56,10 +56,46 @@ b1No2C = nameAction "wool_b1No2C" $ do
     makeAlertableCall (T.Bid 2 T.Clubs) "both majors"
 
 
+b1No2C2H :: Action
+b1No2C2H = nameAction "wool_b1No2C2H" $ do
+    -- If your major suits are the same length, bid the better one. That's
+    -- slightly hard to code up, so just avoid those situations entirely.
+    T.Hearts `longerThan` T.Spades
+    makeCall $ T.Bid 2 T.Hearts
+
+
+b1No2C2S :: Action
+b1No2C2S = nameAction "wool_b1No2C2S" $ do
+    -- If your major suits are the same length, bid the better one. That's
+    -- slightly hard to code up, so just avoid those situations entirely.
+    T.Spades `longerThan` T.Hearts
+    makeCall $ T.Bid 2 T.Spades
+
+
 b1No2D :: Action
 b1No2D = nameAction "wool_b1No2D" $ do
-    alternatives [singleSuited T.Hearts, singleSuited T.Spades]
+    alternatives [b1No2D2HP, b1No2D2H2S]
     makeAlertableCall (T.Bid 2 T.Diamonds) "one long major"
+
+
+b1No2D2H :: Action
+b1No2D2H = nameAction "wool_b1No2D2H" $ do
+    -- If you've got your own long suit, you might bid it. To prevent users from
+    -- being tempted to do that, make sure there aren't long suits.
+    forEach T.allSuits (`maxSuitLength` 5)
+    makeAlertableCall (T.Bid 2 T.Hearts) "pass or correct"
+
+
+b1No2D2HP :: Action
+b1No2D2HP = nameAction "wool_b1No2D2HP" $ do
+    singleSuited T.Hearts
+    makeCall T.Pass
+
+
+b1No2D2H2S :: Action
+b1No2D2H2S = nameAction "wool_b1No2D2H2S" $ do
+    singleSuited T.Spades
+    makeCall $ T.Bid 2 T.Spades
 
 
 b1No2H :: Action
