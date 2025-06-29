a4b4efc9e19f4ea82258fef18552df68484d57f1
Alan Davidson <alan.davidson@gmail.com>
2025-06-29 16:17:04 -0400

    be more lenient about trailing whitespace. Do I need to build a proper parser? Hopefully not

--- debugger/Main.hs
+++ debugger/Main.hs
@@ -6,6 +6,7 @@
 import Control.Monad(when)
 import Control.Monad.Trans.State.Strict(runState, runStateT)
 import Data.List.Utils(join, split)
+import Data.String.Utils(strip)
 -- We import the Internal implementation of the RNG directly because it has
 -- implementations of Read and ways to get at the inner guts of the StdGen that
 -- the non-Internal version does not have. If we ever upgrade to a newer version
@@ -24,13 +25,13 @@ import Topic(Topic(..), choose)
 -- wool.2D StdGen {unStdGen = SMGen 14968322863915291149 13964134228407631271}
 parse :: String -> (String, String, StdGen)
 parse input = let
-    pieces = split " " input
+    pieces = split " " . strip $ input
     fullSitName = head pieces
     -- Confusingly, although StdGen is Show, it is not Read but all its internal
     -- pieces are. So, parse it more manually. We call tail 4 times to take off
     -- the topic name and "StdGen {unStdGen =", join the rest together, and then
-    -- call init twice to remove the newline and '}' from the end.
-    innerRngString = init . init . join " " . tail . tail . tail . tail $ pieces
+    -- call init to remove '}' from the end.
+    innerRngString = init . join " " . tail . tail . tail . tail $ pieces
     rng = StdGen {unStdGen = read innerRngString}
     namePieces = split "." fullSitName
     targetName = head namePieces
