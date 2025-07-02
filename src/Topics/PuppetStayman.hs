60be206b561f066f19885706885038d39b3d9999
Alan Davidson <alan.davidson@gmail.com>
2025-07-02 00:09:59 -0400

    continued work

--- src/Topics/PuppetStayman.hs
+++ src/Topics/PuppetStayman.hs
@@ -2,21 +2,10 @@ module Topics.PuppetStayman(topic) where
 
 import qualified Bids.PuppetStayman as P
 import CommonBids(setOpener)
-import Output((.+))
+import Output((.+), Punct(..))
 import Situation(situation, (<~))
 import qualified Terminology as T
 import Topic(Topic, wrap, stdWrap, wrapDlr, Situations, makeTopic)
-
-
--- Initial 3C
--- Initial 3C, even if 3-2 in majors
--- 5-card major rebids
--- 4-card major rebids
--- no major rebids
--- 5-card major raises
--- Texas transfers over 3N
--- smolen-like re-responses
--- opener sets the contract after smol
 
 
 threeClubs :: Situations
@@ -29,7 +18,7 @@ threeClubs = let
         explanation =
             "Partner has opened " .+ T.Bid 2 T.Notrump .+ ". We've got " .+
             "game-forcing strength, and it's possible we've got a " .+
-            "major-suit fit. Initiate Puppet Stayman."
+            "major-suit fit. Initiate puppet Stayman."
         in situation "3C" action P.b2N3C explanation
   in
     stdWrap sit
@@ -46,16 +35,62 @@ fiveCardMajor = let
             P.noInterference
         explanation =
             "We opened " .+ T.Bid 2 T.Notrump .+ ", and partner bid " .+
-            "Puppet Stayman. We've got a 5-card major, so bid it naturally. " .+
+            "puppet Stayman. We've got a 5-card major, so bid it naturally. " .+
             "Partner will place the contract from there."
         in situation "5M" action major explanation
   in
     wrapDlr $ return sit <~ [P.b2N3C3H, P.b2N3C3S]
 
 
+fourCardMajor :: Situations
+fourCardMajor = let
+    sit = let
+        action = do
+            setOpener T.South
+            P.b2N
+            P.noInterference
+            P.b2N3C
+            P.noInterference
+        explanation =
+            "We opened " .+ T.Bid 2 T.Notrump .+ ", and partner bid " .+
+            "puppet Stayman. We don't have a 5-card major, but do have a " .+
+            "4-card major. Bid " .+ P.b2N3C3D .+ " to show this."
+        in situation "4M" action P.b2N3C3D explanation
+  in
+    stdWrap sit
+
+
+noMajor :: Situations
+noMajor = let
+    sit = let
+        action = do
+            setOpener T.South
+            P.b2N
+            P.noInterference
+            P.b2N3C
+            P.noInterference
+        explanation =
+            "We opened " .+ T.Bid 2 T.Notrump .+ ", and partner bid " .+
+            "puppet Stayman. We don't even have a 4-card major: go " .+
+            "straight to " .+ P.b2N3C3N .+ ". This often ends the auction, " .+
+            "though partner might still make a Texas transfer if they're " .+
+            "6" .+ NDash .+ "4 in the majors."
+        in situation "NM" action P.b2N3C3N explanation
+  in
+    stdWrap sit
+
+
+-- Initial 3C, even if 3-2 in majors
+-- 5-card major raises
+-- Texas transfers over 3N
+-- smolen-like re-responses
+-- opener sets the contract after smol
+
+
 topic :: Topic
-topic = makeTopic ("Puppet Stayman over " .+ T.Bid 2 T.Notrump) "pup" situations
+topic = makeTopic ("puppet Stayman over " .+ T.Bid 2 T.Notrump) "pup" situations
   where
     situations = wrap [ threeClubs
-                      , fiveCardMajor
+                      , wrap [fiveCardMajor, fourCardMajor, noMajor]
+                      , noMajor
                       ]
