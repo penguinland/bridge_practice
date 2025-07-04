8ebb0312555fa13978a4807594d86e5692fbef7e
Alan Davidson <alan.davidson@gmail.com>
2025-07-04 19:38:14 -0400

    more work

--- src/Bids/TakeoutDoubles.hs
+++ src/Bids/TakeoutDoubles.hs
@@ -1,5 +1,6 @@
 module Bids.TakeoutDoubles(
     powerDouble
+  , responderPasses
   , b1C  -- Re-exported from StandardOpenings
   , b1CoX
   , b1CoXo1D
@@ -27,12 +28,13 @@ module Bids.TakeoutDoubles(
 ) where
 
 -- TODO: include jumps to show stronger hands.
+-- TODO: include passes that convert the takeout double to penalty?
 
 import Control.Monad(when)
 
 import Action(Action)
 import Bids.StandardOpenings(b1C, b1D, b1H, b1S)
-import EDSL(minSuitLength, maxSuitLength, makeCall, pointRange, soundHolding, 
+import EDSL(minSuitLength, maxSuitLength, makeCall, pointRange, soundHolding,
             forEach, nameAction, alternatives, strongerThan, hasStopper,
             balancedHand, longerThan, forbid)
 import qualified Terminology as T
@@ -45,6 +47,17 @@ overcall1N_ oppsSuit = nameAction ("b1" ++ T.suitLetter oppsSuit ++ "o1N") $ do
     pointRange 16 18
     hasStopper oppsSuit
     makeCall $ T.Bid 1 T.Notrump
+
+
+responderPasses :: T.Suit -> Action
+responderPasses partnerSuit = nameAction bidName $ do
+    pointRange 0 6
+    maxSuitLength partnerSuit 2
+    forEach (filter (> partnerSuit) T.allSuits) (`maxSuitLength` 4)
+    forEach (filter (< partnerSuit) T.allSuits) (`maxSuitLength` 5)
+    makeCall T.Pass
+  where
+    bidName = "b1" ++ T.suitLetter partnerSuit ++ "oXP"
 
 
 powerDouble :: T.Suit -> Action
@@ -86,7 +99,7 @@ b1SoX = takeoutDouble "b1SoX" T.Spades
 
 minimumResponse_ :: T.Suit -> T.Suit -> Int -> Action
 minimumResponse_ oppsSuit ourSuit level = do
-    forEach T.allSuits (ourSuit `strongerThan`)
+    forEach (T.otherSuits ourSuit) (ourSuit `strongerThan`)
     pointRange 0 10
     when (ourSuit > oppsSuit) (pointRange 0 7)
     makeCall $ T.Bid level ourSuit
@@ -97,10 +110,13 @@ b1CoXo1D = nameAction "b1CoXo1D" $ do
     -- the major, I think. Same if you were 4-4.
     T.Diamonds `longerThan` T.Hearts
     T.Diamonds `longerThan` T.Spades
+    forbid b1CoXo1N
     minimumResponse_ T.Clubs T.Diamonds 1
 
 b1CoXo1H :: Action
 b1CoXo1H = nameAction "b1CoXo1H" $ do
+    -- TODO: would you really prefer to bid 1N over 1M? Maybe not...
+    forbid b1CoXo1N
     -- TODO: if you're 5-5 in the majors, do you always want to bid the stronger
     -- one, or would you perhaps want to bid the cheaper one or the one that
     -- prevents a reverse later? I dunno, figure it out later.
@@ -108,18 +124,22 @@ b1CoXo1H = nameAction "b1CoXo1H" $ do
 
 b1CoXo1S :: Action
 b1CoXo1S = nameAction "b1CoXo1S" $ do
+    forbid b1CoXo1N
     minimumResponse_ T.Clubs T.Spades 1
 
 b1DoXo1H :: Action
 b1DoXo1H = nameAction "b1DoXo1H" $ do
+    forbid b1DoXo1N
     minimumResponse_ T.Diamonds T.Hearts 1
 
 b1DoXo1S :: Action
 b1DoXo1S = nameAction "b1DoXo1S" $ do
+    forbid b1DoXo1N
     minimumResponse_ T.Diamonds T.Spades 1
 
 b1DoXo2C :: Action
 b1DoXo2C = nameAction "b1DoXo2C" $ do
+    forbid b1DoXo1N
     -- If you were 5-5 in clubs and a major, definitely bid the major, even if
     -- the clubs were slightly stronger. Same if you were 4-4.
     T.Clubs `longerThan` T.Hearts
@@ -128,30 +148,36 @@ b1DoXo2C = nameAction "b1DoXo2C" $ do
 
 b1HoXo1S :: Action
 b1HoXo1S = nameAction "b1HoXo1S" $ do
+    forbid b1HoXo1N
     minimumResponse_ T.Hearts T.Spades 1
 
 b1HoXo2C :: Action
 b1HoXo2C = nameAction "b1HoXo2C" $ do
+    forbid b1HoXo1N
     T.Clubs `longerThan` T.Spades
     minimumResponse_ T.Hearts T.Clubs 2
 
 b1HoXo2D :: Action
 b1HoXo2D = nameAction "b1HoXo2D" $ do
+    forbid b1HoXo1N
     T.Diamonds `longerThan` T.Spades
     minimumResponse_ T.Hearts T.Diamonds 2
 
 b1SoXo2C :: Action
 b1SoXo2C = nameAction "b1HoXo2C" $ do
+    forbid b1SoXo1N
     T.Clubs `longerThan` T.Hearts
     minimumResponse_ T.Spades T.Clubs 2
 
 b1SoXo2D :: Action
 b1SoXo2D = nameAction "b1HoXo2D" $ do
+    forbid b1SoXo1N
     T.Diamonds `longerThan` T.Hearts
     minimumResponse_ T.Spades T.Diamonds 2
 
 b1SoXo2H :: Action
 b1SoXo2H = nameAction "b1HoXo2H" $ do
+    forbid b1SoXo1N
     minimumResponse_ T.Spades T.Hearts 2
 
 
--- src/Topics/TakeoutDoubles.hs
+++ src/Topics/TakeoutDoubles.hs
@@ -30,13 +30,69 @@ makeTox = let
                             ]
 
 
--- make a power double
--- bid a suit in response to partner's takeout double
+makePowerX :: Situations
+makePowerX = let
+    sit (opener, openerSuit) = let
+        action = do
+            setOpener T.East
+            opener
+        explanation =
+            "The opponents have opened the bidding, and we've got a super " .+
+            "strong hand. To convey this extra strength, start with a " .+
+            "power double. Partner will think it's a takeout double, but " .+
+            "instead of supporting their suit, we will bid our own suit " .+
+            "(or notrump) later, and then partner will realize how powerful " .+
+            "our hand is."
+      in situation "powx" action (B.powerDouble openerSuit) explanation
+  in
+    wrap $ return sit <~ [ (B.b1C, T.Clubs)
+                         , (B.b1D, T.Diamonds)
+                         , (B.b1H, T.Hearts)
+                         , (B.b1S, T.Spades)
+                         ]
+                      -- To make a power double, you must be an unpassed hand.
+                      <~ [T.West, T.North, T.East]
+                      <~ T.allVulnerabilities
+
+
+advanceSuit :: Situations
+advanceSuit = let
+    sit (opener, openerSuit, overcall, advance) = let
+        action = do
+            setOpener T.West
+            _ <- opener
+            _ <- overcall
+            B.responderPasses openerSuit
+        explanation =
+            "The opponents have opened the bidding, partner has made " .+
+            "a takeout double, and the next player has passed. We need " .+
+            "to take the double out by bidding our favorite suit. Do this " .+
+            "even with 0 high card points! Partner has support for us, " .+
+            "and it beats letting the opponents play doubled at the 1 level."
+      in situation "advS" action advance explanation
+  in
+    wrapDlr $ return sit <~ [ (B.b1C, T.Clubs,    B.b1CoX, B.b1CoXo1D)
+                            , (B.b1C, T.Clubs,    B.b1CoX, B.b1CoXo1H)
+                            , (B.b1C, T.Clubs,    B.b1CoX, B.b1CoXo1S)
+                            , (B.b1D, T.Diamonds, B.b1DoX, B.b1DoXo1H)
+                            , (B.b1D, T.Diamonds, B.b1DoX, B.b1DoXo1S)
+                            , (B.b1D, T.Diamonds, B.b1DoX, B.b1DoXo2C)
+                            , (B.b1H, T.Hearts,   B.b1HoX, B.b1HoXo1S)
+                            , (B.b1H, T.Hearts,   B.b1HoX, B.b1HoXo2C)
+                            , (B.b1H, T.Hearts,   B.b1HoX, B.b1HoXo2D)
+                            , (B.b1S, T.Spades,   B.b1SoX, B.b1SoXo2C)
+                            , (B.b1S, T.Spades,   B.b1SoX, B.b1SoXo2D)
+                            , (B.b1S, T.Spades,   B.b1SoX, B.b1SoXo2H)
+                            ]
+
+
 -- bid 1N in response to partner's takeout double
+-- convert the takeout double to penalty
 
 
 topic :: Topic
 topic = makeTopic "takeout doubles of 1-level suit openings" "tox" situations
   where
-    situations = wrap [ makeTox
+    situations = wrap [ wrap [makeTox, makeTox, makeTox, makeTox, makePowerX]
+                      , advanceSuit
                       ]
