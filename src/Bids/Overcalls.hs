2a61240d36b967ebfca380a54a5ad4af40ffea2b
Alan Davidson <alan.davidson@gmail.com>
2025-07-06 13:31:26 -0400

    finish the immediate overcalls

--- src/Bids/Overcalls.hs
+++ src/Bids/Overcalls.hs
@@ -1,8 +1,8 @@
 module Bids.Overcalls(
     b1C  -- re-exported from StandardOpenings
   , b1C1D
---  , b1C1H
---  , b1C1S
+  , b1C1H
+  , b1C1S
 --  , b1C1N
 --  , b1C2D
 --  , b1C2H
@@ -14,10 +14,10 @@ module Bids.Overcalls(
 --  , b1C4H
 --  , b1C4S
   , b1D  -- re-exported from StandardOpenings
---  , b1D1H
---  , b1D1S
+  , b1D1H
+  , b1D1S
 --  , b1D1N
---  , b1D2C
+  , b1D2C
 --  , b1D2H
 --  , b1D2S
 --  , b1D3C
@@ -27,10 +27,10 @@ module Bids.Overcalls(
 --  , b1D4H
 --  , b1D4S
   , b1H  -- re-exported from StandardOpenings
---  , b1H1S
+  , b1H1S
 --  , b1H1N
---  , b1H2C
---  , b1H2D
+  , b1H2C
+  , b1H2D
 --  , b1H2S
 --  , b1H3C
 --  , b1H3D
@@ -40,9 +40,9 @@ module Bids.Overcalls(
 --  , b1H4S
   , b1S  -- re-exported from StandardOpenings
 --  , b1S1N
---  , b1S2C
+  , b1S2C
   , b1S2D
---  , b1S2H
+  , b1S2H
 --  , b1S3C
 --  , b1S3D
 --  , b1S3H
@@ -87,7 +87,61 @@ b1C1D = nameAction "b1C1D" $ do
     oneLevelOvercall_ T.Diamonds
 
 
-b1S2D :: Action
-b1S2D = nameAction "b1C1D" $ do
-    T.Diamonds `longerThan` T.Hearts
+b1C1H :: Action
+b1C1H = nameAction "b1C1H" $ do
+    T.Hearts `longerThan` T.Spades
+    oneLevelOvercall_ T.Hearts
+
+
+b1C1S :: Action
+b1C1S = nameAction "b1C1S" $ do
+    oneLevelOvercall_ T.Spades
+
+
+b1D1H :: Action
+b1D1H = nameAction "b1D1H" $ do
+    T.Hearts `longerThan` T.Spades
+    oneLevelOvercall_ T.Hearts
+
+
+b1D1S :: Action
+b1D1S = nameAction "b1D1S" $ do
+    oneLevelOvercall_ T.Spades
+
+
+b1D2C :: Action
+b1D2C = nameAction "b1D2C" $ do
+    T.Clubs `longerThan` T.Hearts
+    T.Clubs `longerThan` T.Spades
+    twoLevelOvercall_ T.Clubs
+
+
+b1H1S :: Action
+b1H1S = nameAction "b1H1S" $ do
+    oneLevelOvercall_ T.Spades
+
+
+b1H2C :: Action
+b1H2C = nameAction "b1H2C" $ do
+    T.Clubs `longerThan` T.Spades
+    twoLevelOvercall_ T.Clubs
+
+
+b1H2D :: Action
+b1H2D = nameAction "b1H2D" $ do
     twoLevelOvercall_ T.Diamonds
+
+
+b1S2C :: Action
+b1S2C = nameAction "b1S2C" $ do
+    twoLevelOvercall_ T.Clubs
+
+
+b1S2D :: Action
+b1S2D = nameAction "b1S2D" $ do
+    twoLevelOvercall_ T.Diamonds
+
+
+b1S2H :: Action
+b1S2H = nameAction "b1S2H" $ do
+    twoLevelOvercall_ T.Spades
