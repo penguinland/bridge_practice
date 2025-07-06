6155aaef07c75a91f3945092a4f4cf3bc0bdd1e6
Alan Davidson <alan.davidson@gmail.com>
2025-07-06 13:39:13 -0400

    fix naming conventions, add 1N overcalls

--- src/Bids/Overcalls.hs
+++ src/Bids/Overcalls.hs
@@ -1,61 +1,61 @@
 module Bids.Overcalls(
     b1C  -- re-exported from StandardOpenings
-  , b1C1D
-  , b1C1H
-  , b1C1S
---  , b1C1N
---  , b1C2D
---  , b1C2H
---  , b1C2S
---  , b1C3D
---  , b1C3H
---  , b1C3S
---  , b1C4D
---  , b1C4H
---  , b1C4S
+  , b1Co1D
+  , b1Co1H
+  , b1Co1S
+  , b1Co1N
+--  , b1Co2D
+--  , b1Co2H
+--  , b1Co2S
+--  , b1Co3D
+--  , b1Co3H
+--  , b1Co3S
+--  , b1Co4D
+--  , b1Co4H
+--  , b1Co4S
   , b1D  -- re-exported from StandardOpenings
-  , b1D1H
-  , b1D1S
---  , b1D1N
-  , b1D2C
---  , b1D2H
---  , b1D2S
---  , b1D3C
---  , b1D3H
---  , b1D3S
---  , b1D4C
---  , b1D4H
---  , b1D4S
+  , b1Do1H
+  , b1Do1S
+  , b1Do1N
+  , b1Do2C
+--  , b1Do2H
+--  , b1Do2S
+--  , b1Do3C
+--  , b1Do3H
+--  , b1Do3S
+--  , b1Do4C
+--  , b1Do4H
+--  , b1Do4S
   , b1H  -- re-exported from StandardOpenings
-  , b1H1S
---  , b1H1N
-  , b1H2C
-  , b1H2D
---  , b1H2S
---  , b1H3C
---  , b1H3D
---  , b1H3S
---  , b1H4C
---  , b1H4D
---  , b1H4S
+  , b1Ho1S
+  , b1Ho1N
+  , b1Ho2C
+  , b1Ho2D
+--  , b1Ho2S
+--  , b1Ho3C
+--  , b1Ho3D
+--  , b1Ho3S
+--  , b1Ho4C
+--  , b1Ho4D
+--  , b1Ho4S
   , b1S  -- re-exported from StandardOpenings
---  , b1S1N
-  , b1S2C
-  , b1S2D
-  , b1S2H
---  , b1S3C
---  , b1S3D
---  , b1S3H
---  , b1S4C
---  , b1S4D
---  , b1S4H
+  , b1So1N
+  , b1So2C
+  , b1So2D
+  , b1So2H
+--  , b1So3C
+--  , b1So3D
+--  , b1So3H
+--  , b1So4C
+--  , b1So4D
+--  , b1So4H
 ) where
 
 
 import Action(Action)
 import Bids.StandardOpenings(b1C, b1D, b1H, b1S)
-import EDSL(makeCall, forEach, minSuitLength, longerThan, atLeastAsLong,
-            nameAction, pointRange, soundHolding)
+import EDSL(makeCall, minSuitLength, maxSuitLength, longerThan, atLeastAsLong,
+            forEach, nameAction, pointRange, soundHolding, balancedHand)
 import qualified Terminology as T
 
 
@@ -80,68 +80,97 @@ twoLevelOvercall_ suit = do
     makeCall $ T.Bid 2 suit
 
 
-b1C1D :: Action
-b1C1D = nameAction "b1C1D" $ do
+overcall1N_ :: T.Suit -> Action
+overcall1N_ suit = do
+    balancedHand
+    pointRange 16 18
+    soundHolding suit
+    forEach (T.otherSuits suit) (`maxSuitLength` 4)
+    makeCall $ T.Bid 1 T.Notrump
+
+
+b1Co1D :: Action
+b1Co1D = nameAction "b1Co1D" $ do
     T.Diamonds `longerThan` T.Hearts
     T.Diamonds `longerThan` T.Spades
     oneLevelOvercall_ T.Diamonds
 
 
-b1C1H :: Action
-b1C1H = nameAction "b1C1H" $ do
+b1Co1H :: Action
+b1Co1H = nameAction "b1Co1H" $ do
     T.Hearts `longerThan` T.Spades
     oneLevelOvercall_ T.Hearts
 
 
-b1C1S :: Action
-b1C1S = nameAction "b1C1S" $ do
+b1Co1S :: Action
+b1Co1S = nameAction "b1Co1S" $ do
     oneLevelOvercall_ T.Spades
 
 
-b1D1H :: Action
-b1D1H = nameAction "b1D1H" $ do
+b1Do1H :: Action
+b1Do1H = nameAction "b1Do1H" $ do
     T.Hearts `longerThan` T.Spades
     oneLevelOvercall_ T.Hearts
 
 
-b1D1S :: Action
-b1D1S = nameAction "b1D1S" $ do
+b1Do1S :: Action
+b1Do1S = nameAction "b1Do1S" $ do
     oneLevelOvercall_ T.Spades
 
 
-b1D2C :: Action
-b1D2C = nameAction "b1D2C" $ do
+b1Do2C :: Action
+b1Do2C = nameAction "b1Do2C" $ do
     T.Clubs `longerThan` T.Hearts
     T.Clubs `longerThan` T.Spades
     twoLevelOvercall_ T.Clubs
 
 
-b1H1S :: Action
-b1H1S = nameAction "b1H1S" $ do
+b1Ho1S :: Action
+b1Ho1S = nameAction "b1Ho1S" $ do
     oneLevelOvercall_ T.Spades
 
 
-b1H2C :: Action
-b1H2C = nameAction "b1H2C" $ do
+b1Ho2C :: Action
+b1Ho2C = nameAction "b1Ho2C" $ do
     T.Clubs `longerThan` T.Spades
     twoLevelOvercall_ T.Clubs
 
 
-b1H2D :: Action
-b1H2D = nameAction "b1H2D" $ do
+b1Ho2D :: Action
+b1Ho2D = nameAction "b1Ho2D" $ do
     twoLevelOvercall_ T.Diamonds
 
 
-b1S2C :: Action
-b1S2C = nameAction "b1S2C" $ do
+b1So2C :: Action
+b1So2C = nameAction "b1So2C" $ do
     twoLevelOvercall_ T.Clubs
 
 
-b1S2D :: Action
-b1S2D = nameAction "b1S2D" $ do
+b1So2D :: Action
+b1So2D = nameAction "b1So2D" $ do
     twoLevelOvercall_ T.Diamonds
 
 
-b1S2H :: Action
-b1S2H = nameAction "b1S2H" $ do
+b1So2H :: Action
+b1So2H = nameAction "b1So2H" $ do
     twoLevelOvercall_ T.Spades
+
+
+b1Co1N :: Action
+b1Co1N = nameAction "b1Co1N" $ do
+    overcall1N_ T.Clubs
+
+
+b1Do1N :: Action
+b1Do1N = nameAction "b1Do1N" $ do
+    overcall1N_ T.Diamonds
+
+
+b1Ho1N :: Action
+b1Ho1N = nameAction "b1Ho1N" $ do
+    overcall1N_ T.Hearts
+
+
+b1So1N :: Action
+b1So1N = nameAction "b1So1N" $ do
+    overcall1N_ T.Spades
