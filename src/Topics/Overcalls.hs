3852262cd3f37291c0f45b7c2697991a41594bd0
Alan Davidson <alan.davidson@gmail.com>
2025-07-06 22:03:02 -0400

    Add a situation for 2-level overcalls

--- src/Topics/Overcalls.hs
+++ src/Topics/Overcalls.hs
@@ -22,9 +22,30 @@ oneLevelOvercall = let
     wrapDlr $ return sit <~ [ (B.b1C, B.b1Co1D)
                             , (B.b1C, B.b1Co1H)
                             , (B.b1C, B.b1Co1S)
-                            , (B.b1D, B.b1Co1H)
-                            , (B.b1D, B.b1Co1S)
-                            , (B.b1H, B.b1Co1S)
+                            , (B.b1D, B.b1Do1H)
+                            , (B.b1D, B.b1Do1S)
+                            , (B.b1H, B.b1Ho1S)
+                            ]
+
+
+twoLevelOvercall :: Situations
+twoLevelOvercall = let
+    sit (opening, overcall) = let
+        action = do
+            setOpener T.East
+            opening
+        explanation =
+            "RHO has opened the bidding. We can't overcall at the 1 level, " .+
+            "but we've got enough extra strength that we can overcall at " .+
+            "the 2 level instead."
+      in situation "o2" action overcall explanation
+  in
+    wrapDlr $ return sit <~ [ (B.b1D, B.b1Do2C)
+                            , (B.b1H, B.b1Ho2C)
+                            , (B.b1H, B.b1Ho2D)
+                            , (B.b1S, B.b1So2C)
+                            , (B.b1S, B.b1So2D)
+                            , (B.b1S, B.b1So2H)
                             ]
 
 
@@ -32,4 +53,5 @@ topic :: Topic
 topic = makeTopic "immediate overcalls" "overC" situations
   where
     situations = wrap [ oneLevelOvercall
+                      , twoLevelOvercall
                       ]
