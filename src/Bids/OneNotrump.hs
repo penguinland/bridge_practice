d357a9675619c8f35fd6a668dcf862f18dc296be
Alan Davidson <alan.davidson@gmail.com>
2025-09-07 22:05:33 -0400

    define immediate responses

--- src/Bids/OneNotrump.hs
+++ src/Bids/OneNotrump.hs
@@ -49,6 +49,9 @@ module Bids.OneNotrump(
   , b1N2H2S3S
   , b1N2H2S4S
   , b1N2H3S
+  , b1N3D
+  , b1N3H
+  , b1N3S
   , b1N4D
   , b1N4D4H
   , b1N4H
@@ -549,3 +552,36 @@ b1N2H2S3S = nameAction "b1N2H2S3S" $ do
     -- hand you might upgrade to game-forcing anyway. Avoid these situations.
     forEach [T.Clubs, T.Diamonds, T.Hearts] (`maxSuitLength` 3)
     makeCall $ T.Bid 3 T.Spades
+
+
+-- Common 3-level responses
+
+-- TODO: some folks play 3C is (low-info) Puppet Stayman, others play it's 5-5
+-- in the minors and invitational. Define these things eventually.
+
+
+b1N3D :: Action
+b1N3D = nameAction "b1N3D" $ do
+    gameForcing
+    forEach T.minorSuits (`minSuitLength` 5)
+    makeAlertableCall (T.Bid 3 T.Diamonds) "at least 5-5 in the minors, GF"
+
+
+b1N3H :: Action
+b1N3H = nameAction "b1N3H" $ do
+    gameForcing
+    suitLength T.Hearts 1
+    suitLength T.Spades 3
+    forEach T.minorSuits (`minSuitLength` 4)
+    forEach T.minorSuits (`maxSuitLength` 5)
+    makeAlertableCall (T.Bid 3 T.Hearts) "splinter: 31(54) shape, GF"
+
+
+b1N3S :: Action
+b1N3S = nameAction "b1N3S" $ do
+    gameForcing
+    suitLength T.Spades 1
+    suitLength T.Hearts 3
+    forEach T.minorSuits (`minSuitLength` 4)
+    forEach T.minorSuits (`maxSuitLength` 5)
+    makeAlertableCall (T.Bid 3 T.Spades) "splinter: 13(54) shape, GF"
