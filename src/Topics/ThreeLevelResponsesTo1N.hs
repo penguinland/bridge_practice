963c69a24cccc265f915f26846cd5083644de3ae
Alan Davidson <alan.davidson@gmail.com>
2025-09-08 00:19:53 -0400

    first stab at the topic

---
+++ src/Topics/ThreeLevelResponsesTo1N.hs
@@ -0,0 +1,51 @@
+module Topics.ThreeLevelResponsesTo1N(topic) where
+
+import qualified Bids.OneNotrump as B
+import CommonBids(setOpener)
+import EDSL(alternatives)
+import Output(Punct(..), (.+))
+import Situation(situation, (<~))
+import qualified Terminology as T
+import Topic(Topic, stdWrap, stdWrapSE, wrap, wrapDlr, Situations, makeTopic)
+
+
+splinter :: Situations
+splinter = let
+    sit bid = let
+        action = do
+            setOpener T.North
+            B.b1N
+            B.noInterference
+        explanation =
+            "Partner opened a strong " .+ B.b1N .+ ". With game-forcing " .+
+            "strength, 5-4 in the minors and 3-1 in the majors, make a " .+
+            "splinter bid. Partner will place the contract from here."
+      in situation "splnt" action bid explanation
+  in
+    wrapDlr $ return sit <~ [B.b1N3H, B.b1N3S]
+
+bothMinors :: Situations
+bothMinors = let
+    sit = let
+        action = do
+            setOpener T.North
+            B.b1N
+            B.noInterference
+        explanation =
+            "Partner opened a strong " .+ B.b1N .+ ". With game-forcing " .+
+            "strength and at least 5-5 in the minors, jump in diamonds. " .+
+            "Opener can place the strain from here, either by signing off " .+
+            "in " .+ B.b1N3N .+ ", or setting trump at the 4 level. We'll " .+
+            "usually just raise a 4-level bid to game in the minor, but " .+
+            "could explore slam with sufficient strength."
+      in situation "m55" action B.b1N3D explanation
+  in
+    wrapDlr $ return sit
+
+
+topic :: Topic
+topic = makeTopic ("Common 3-level responses to" .+ B.b1N) "1N3X" situations
+  where
+    situations = wrap [ splinter
+                      , bothMinors
+                      ]
