07a0455cccbc1258aa47e705655ec8cabfbfb4d3
Alan Davidson <alan.davidson@gmail.com>
2025-10-16 21:27:39 -0400

    start on 2/1 situations (#161)
    This is the bare minimum to start, but at least it's something.
    
    * start on 2/1 situations
    
    * todos to implement later
    
    * another situation

---
+++ src/Bids/TwoOverOne.hs
@@ -0,0 +1,91 @@
+module Bids.TwoOverOne(
+  -- Opening bids are re-exported from StandardOpenings.hs
+    b1D
+  , b1D2C
+  , b1H
+  , b1H2C
+  , b1H2D
+  , b1S
+  , b1S2C
+  , b1S2D
+  , b1S2H
+) where
+
+import Action(Action)
+import qualified EDSL as E
+import qualified Terminology as T
+
+import Bids.StandardOpenings(b1D, b1H, b1S)
+
+
+_gameForcing :: Action
+_gameForcing = E.pointRange 12 40
+
+
+b1D2C :: Action
+b1D2C = E.nameAction "b1D2C" $ do
+    _gameForcing
+    E.forEach T.majorSuits (`E.maxSuitLength` 3)
+    E.minSuitLength T.Clubs 5
+    -- If you're 5-5 in the minors, probably support partner's suit
+    T.Clubs `E.longerThan` T.Diamonds
+    E.forbid E.balancedHand
+    E.makeCall $ T.Bid 2 T.Clubs
+
+
+b1H2C :: Action
+b1H2C = E.nameAction "b1H2C" $ do
+    _gameForcing
+    E.maxSuitLength T.Spades 3
+    E.maxSuitLength T.Hearts 3
+    E.minSuitLength T.Clubs 4
+    -- TODO: With 5-5 in the minors, which should you bid first?
+    T.Clubs `E.longerThan` T.Diamonds
+    E.forbid E.balancedHand
+    E.makeCall $ T.Bid 2 T.Clubs
+
+
+b1H2D :: Action
+b1H2D = E.nameAction "b1H2D" $ do
+    _gameForcing
+    E.maxSuitLength T.Spades 3
+    E.maxSuitLength T.Hearts 3
+    E.minSuitLength T.Diamonds 4
+    -- TODO: With 5-5 in the minors, which should you bid first?
+    T.Diamonds `E.longerThan` T.Clubs
+    E.forbid E.balancedHand
+    E.makeCall $ T.Bid 2 T.Diamonds
+
+
+
+b1S2C :: Action
+b1S2C = E.nameAction "b1S2C" $ do
+    _gameForcing
+    E.maxSuitLength T.Spades 3
+    E.minSuitLength T.Clubs 4
+    -- With 5-5 in the minors, start with diamonds. No need to reverse if we're
+    -- already in a GF auction.
+    T.Clubs `E.longerThan` T.Diamonds
+    E.forbid E.balancedHand
+    E.makeCall $ T.Bid 2 T.Clubs
+
+
+b1S2D :: Action
+b1S2D = E.nameAction "b1S2D" $ do
+    _gameForcing
+    E.maxSuitLength T.Spades 3
+    E.minSuitLength T.Diamonds 4
+    T.Diamonds `E.atLeastAsLong` T.Clubs
+    E.forbid E.balancedHand
+    E.makeCall $ T.Bid 2 T.Diamonds
+
+
+b1S2H :: Action
+b1S2H = E.nameAction "b1S2H" $ do
+    _gameForcing
+    E.maxSuitLength T.Spades 3
+    E.minSuitLength T.Hearts 5
+    -- If you're 5-5, definitely prefer showing the major first
+    E.forEach T.minorSuits (T.Hearts `E.atLeastAsLong`)
+    E.forbid E.balancedHand
+    E.makeCall $ T.Bid 2 T.Hearts
---
+++ src/Topics/TwoOverOne.hs
@@ -0,0 +1,67 @@
+module Topics.TwoOverOne(topic) where
+
+import qualified Bids.TwoOverOne as TOO
+import CommonBids(setOpener, noInterference)
+import qualified EDSL as E
+import Output((.+))
+import Situation(situation, (<~))
+import qualified Terminology as T
+import Topic(Topic, wrap, wrapNW, Situations, makeTopic)
+
+
+
+twoOverOne :: Situations
+twoOverOne = let
+    sit (openingSuit, openingBid, response) = let
+        action = do
+            setOpener T.North
+            _ <- openingBid
+            noInterference openingSuit
+        explanation =
+            "Partner opened the bidding. We've got game-forcing strength, " .+
+            "so start by making a 2/1 bid. Once partner knows not to pass " .+
+            "too low, we can explore further to find the right fit."
+        in situation "2o1" action response explanation
+  in
+    wrapNW $ return sit <~ [ (T.Diamonds, TOO.b1D, TOO.b1D2C)
+                           , (T.Hearts,   TOO.b1H, TOO.b1H2C)
+                           , (T.Hearts,   TOO.b1H, TOO.b1H2D)
+                           , (T.Spades,   TOO.b1S, TOO.b1S2C)
+                           , (T.Spades,   TOO.b1S, TOO.b1S2D)
+                           , (T.Spades,   TOO.b1S, TOO.b1S2H)
+                           ]
+
+
+threeCardSupport :: Situations
+threeCardSupport = let
+    sit (openingSuit, openingBid, response) = let
+        action = do
+            setOpener T.North
+            _ <- openingBid
+            noInterference openingSuit
+            E.suitLength openingSuit 3
+        explanation =
+            "Partner opened the bidding. We've got game-forcing strength " .+
+            "and 3-card support for their major. Start by making a 2/1 " .+
+            "bid so partner won't pass too low, then show our fit after that."
+        in situation "3M" action response explanation
+  in
+    wrapNW $ return sit <~ [ (T.Hearts, TOO.b1H, TOO.b1H2C)
+                           , (T.Hearts, TOO.b1H, TOO.b1H2D)
+                           , (T.Spades, TOO.b1S, TOO.b1S2C)
+                           , (T.Spades, TOO.b1S, TOO.b1S2D)
+                           , (T.Spades, TOO.b1S, TOO.b1S2H)
+                           ]
+
+
+-- TODO:
+-- opener rebids after responder's 2/1 bid
+-- responder is too weak so bids 1N Forcing
+
+
+topic :: Topic
+topic = makeTopic "2/1 game forcing" "2o1" situations
+  where
+    situations = wrap [ twoOverOne
+                      , threeCardSupport
+                      ]
