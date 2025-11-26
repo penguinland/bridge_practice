351cd07d5b15c2b1670ce397a48432cd664added
Alan Davidson <alan.davidson@gmail.com>
2025-11-26 16:04:16 -0500

    get the new assertion up and going, needs cleanup

--- src/Assertions.hs
+++ src/Assertions.hs
@@ -3,10 +3,24 @@
 -- (e.g., topicList) must be already compiled in a separate file.
 module Assertions where
 
-import Data.Tuple.Utils(fst3)
+import Control.Monad.Extra(mconcatMapM)
+import Data.Tuple.Utils(fst3, thd3)
 
 import CompileTime(staticAssert, duplicatesOf)
+import Output(toMonospace)
+import Situation(sitRef)
 import SupportedTopics(topicList)
+import Topic(collect, topicName)
 
 $(let dups = duplicatesOf . map fst3 $ topicList
   in staticAssert (null dups) ("topic list has duplicate IDs: " ++ show dups))
+
+{-
+$(let assertUniqueNames top =
+    (let dups = duplicatesOf . collect sitRef . topicSituations $ top
+     in staticAssert (null dups) ("topic " ++ topicName top ++ " has duplicate situation names: " ++ show dups))
+  in map (assertUniqueNames . thd3) topicList)
+-}
+
+$(let assertUniqueNames topic = (let dups = (duplicatesOf . collect sitRef $ topic) in staticAssert (null dups) ("duplicate situation names for topic " ++ (toMonospace . topicName $ topic) ++ ": " ++ show dups))
+  in mconcatMapM (assertUniqueNames . thd3) $ topicList)
