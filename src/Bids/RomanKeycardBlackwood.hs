b05e4912f84b2b4b3cd771cdece2e4439a48d022
Alan Davidson <alan.davidson@gmail.com>
2025-11-25 20:39:46 -0500

    naming tweak

--- src/Bids/RomanKeycardBlackwood.hs
+++ src/Bids/RomanKeycardBlackwood.hs
@@ -1,15 +1,15 @@
 module Bids.RomanKeycardBlackwood(
     bRKC4N
-  , b1430RKCH5C
-  , b1430RKCH5D
-  , b3014RKCH5C
-  , b3014RKCH5D
+  , bRKC1430H5C
+  , bRKC1430H5D
+  , bRKC3014H5C
+  , bRKC3014H5D
   , bRKCH5H
   , bRKCH5S
-  , b1430RKCS5C
-  , b1430RKCS5D
-  , b3014RKCS5C
-  , b3014RKCS5D
+  , bRKC1430S5C
+  , bRKC1430S5D
+  , bRKC3014S5C
+  , bRKC3014S5D
   , bRKCS5H
   , bRKCS5S
 ) where
@@ -24,14 +24,14 @@ bRKC4N :: Action
 bRKC4N = E.makeAlertableCall (T.Bid 4 T.Notrump) "(postalert) Keycard Ask"
 
 
-b1430RKCH5C :: Action
-b1430RKCH5C = E.nameAction "RKC1430_H_5C" $ do
+bRKC1430H5C :: Action
+bRKC1430H5C = E.nameAction "RKC1430_H_5C" $ do
     E.keycardCount T.Hearts 1 4
     E.makeAlertableCall (T.Bid 5 T.Clubs) "(postalert) 1 or 4 keycards"
 
 
-b1430RKCH5D :: Action
-b1430RKCH5D = E.nameAction "RKC1430_H_5D" $ do
+bRKC1430H5D :: Action
+bRKC1430H5D = E.nameAction "RKC1430_H_5D" $ do
     E.keycardCount T.Hearts 3 0
     E.makeAlertableCall (T.Bid 5 T.Diamonds) "(postalert) 3 or 0 keycards"
 
@@ -50,14 +50,14 @@ bRKCH5S = E.nameAction "RKC_H_5S" $ do
     E.makeAlertableCall (T.Bid 5 T.Spades) "(postalert) 2 or 5 keycards with Q"
 
 
-b1430RKCS5C :: Action
-b1430RKCS5C = E.nameAction "RKC1430_S_5C" $ do
+bRKC1430S5C :: Action
+bRKC1430S5C = E.nameAction "RKC1430_S_5C" $ do
     E.keycardCount T.Spades 1 4
     E.makeAlertableCall (T.Bid 5 T.Clubs) "(postalert) 1 or 4 keycards"
 
 
-b1430RKCS5D :: Action
-b1430RKCS5D = E.nameAction "RKC1430_S_5D" $ do
+bRKC1430S5D :: Action
+bRKC1430S5D = E.nameAction "RKC1430_S_5D" $ do
     E.keycardCount T.Spades 3 0
     E.makeAlertableCall (T.Bid 5 T.Diamonds) "(postalert) 3 or 0 keycards"
 
@@ -79,22 +79,22 @@ bRKCS5S = E.nameAction "RKC_S_5S" $ do
 -- For RKC3014, reuse as much as possible. To keep changes to the dealer program
 -- while ignoring the bids, forbid forbidding the original.
 
-b3014RKCH5C :: Action
-b3014RKCH5C = E.nameAction "RKC3014_H_5C" $ do
-    E.forbid (E.forbid b1430RKCH5D)
+bRKC3014H5C :: Action
+bRKC3014H5C = E.nameAction "RKC3014_H_5C" $ do
+    E.forbid (E.forbid bRKC1430H5D)
     E.makeAlertableCall (T.Bid 5 T.Clubs) "(postalert) 3 or 0 keycards"
 
-b3014RKCH5D :: Action
-b3014RKCH5D = E.nameAction "RKC3014_H_5D" $ do
-    E.forbid (E.forbid b1430RKCH5C)
+bRKC3014H5D :: Action
+bRKC3014H5D = E.nameAction "RKC3014_H_5D" $ do
+    E.forbid (E.forbid bRKC1430H5C)
     E.makeAlertableCall (T.Bid 5 T.Diamonds) "(postalert) 1 or 4 keycards"
 
-b3014RKCS5C :: Action
-b3014RKCS5C = E.nameAction "RKC3014_S_5C" $ do
-    E.forbid (E.forbid b1430RKCS5D)
+bRKC3014S5C :: Action
+bRKC3014S5C = E.nameAction "RKC3014_S_5C" $ do
+    E.forbid (E.forbid bRKC1430S5D)
     E.makeAlertableCall (T.Bid 5 T.Clubs) "(postalert) 3 or 0 keycards"
 
-b3014RKCS5D :: Action
-b3014RKCS5D = E.nameAction "RKC3014_S_5D" $ do
-    E.forbid (E.forbid b1430RKCS5C)
+bRKC3014S5D :: Action
+bRKC3014S5D = E.nameAction "RKC3014_S_5D" $ do
+    E.forbid (E.forbid bRKC1430S5C)
     E.makeAlertableCall (T.Bid 5 T.Diamonds) "(postalert) 1 or 4 keycards"
