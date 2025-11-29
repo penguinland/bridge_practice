b5c6d367f03256a4651da4bf75fd9ad59cf7894d
Alan Davidson <alan.davidson@gmail.com>
2025-11-29 15:53:02 -0500

    split the setup auctions by trump suit

--- src/Topics/RomanKeycardBlackwood.hs
+++ src/Topics/RomanKeycardBlackwood.hs
@@ -11,19 +11,12 @@ import qualified Terminology as T
 import Topic(Topic, wrap, wrapNW, Situations, makeTopic)
 
 
-setUpAuctions_ :: [Action]
-setUpAuctions_ = [ do J2N.b1H
+setUpAuctionsH :: [Action]
+setUpAuctionsH = [ do J2N.b1H
                       noInterference T.Hearts
                       J2N.b1H2N
                       noInterference T.Hearts
                       J2N.b1H2N4H
-                      makePass
-                      pointRange 19 40
-                 , do J2N.b1S
-                      noInterference T.Spades
-                      J2N.b1S2N
-                      noInterference T.Spades
-                      J2N.b1S2N4S
                       makePass
                       pointRange 19 40
                  , do J2N.b1H
@@ -31,6 +24,16 @@ setUpAuctions_ = [ do J2N.b1H
                       J2N.b1H2N
                       noInterference T.Hearts
                       J2N.b1H2N4D
+                      makePass
+                      pointRange 19 40
+                 ]
+
+setUpAuctionsS :: [Action]
+setUpAuctionsS = [ do J2N.b1S
+                      noInterference T.Spades
+                      J2N.b1S2N
+                      noInterference T.Spades
+                      J2N.b1S2N4S
                       makePass
                       pointRange 19 40
                  , do J2N.b1S
@@ -54,19 +57,19 @@ initiate = let
             "two of them, we'll sign off at the 5 level."
       in situation "init" action RKC.bRKC4N explanation
   in
-    wrapNW $ return sit <~ setUpAuctions_
+    wrapNW $ return sit <~ (setUpAuctionsH ++ setUpAuctionsS)
 
 
 topic1430 :: Topic
 topic1430 = makeTopic "Roman Keycard Blackwood 1430" "RKC1430" situations
   where
     situations = wrap [ initiate
-                      , initiate
+                      --, firstResponse1430
                       ]
 
 topic3014 :: Topic
 topic3014 = makeTopic "Roman Keycard Blackwood 3014" "RKC3014" situations
   where
     situations = wrap [ initiate
-                      , initiate
+                      --, firstResponse3014
                       ]
