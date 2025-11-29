676ec19f6add5c375dd18c60fd4848b52b3343ba
Alan Davidson <alan.davidson@gmail.com>
2025-11-29 15:30:45 -0500

    make starter auctions for RKC

--- src/Topics/RomanKeycardBlackwood.hs
+++ src/Topics/RomanKeycardBlackwood.hs
@@ -1,35 +1,60 @@
-module Topics.RomanKeycardBlackwood(topic) where
+module Topics.RomanKeycardBlackwood(topic1430, topic3014) where
 
+import Action(Action)
 import qualified Bids.RomanKeycardBlackwood as RKC
-import CommonBids(setOpener)
--- TODO: replace makePass with something more intelligent
-import EDSL(makePass, pointRange, suitLength, maxSuitLength, forEach,
-            forbid, semibalancedHand, alternatives)
-import Output((.+), Punct(..))
+import qualified Bids.Jacoby2NT as J2N
+import CommonBids(andNextBidderIs, noInterference)
+import EDSL(makePass, pointRange)
+import Output((.+))
 import Situation(situation, (<~))
 import qualified Terminology as T
-import Topic(Topic, stdWrap, wrap, wrapDlr, wrapNW, wrapSE, Situations,
-             makeTopic)
+import Topic(Topic, wrap, wrapNW, Situations, makeTopic)
 
 
 setUpAuctions_ :: [Action]
-setUpAuctions_ = [
-    ]
+setUpAuctions_ = [ do J2N.b1H
+                      noInterference T.Hearts
+                      J2N.b1H2N
+                      noInterference T.Hearts
+                      J2N.b1H2N4H
+                      makePass
+                      pointRange 19 40
+                 , do J2N.b1S
+                      noInterference T.Spades
+                      J2N.b1S2N
+                      noInterference T.Spades
+                      J2N.b1S2N4S
+                      makePass
+                      pointRange 19 40
+                 , do J2N.b1H
+                      noInterference T.Hearts
+                      J2N.b1H2N
+                      noInterference T.Hearts
+                      J2N.b1H2N4D
+                      makePass
+                      pointRange 19 40
+                 , do J2N.b1S
+                      noInterference T.Spades
+                      J2N.b1S2N
+                      noInterference T.Spades
+                      J2N.b1S2N4H
+                      makePass
+                      pointRange 19 40
+                 ]
 
 
 initiate :: Situations
 initiate = let
     sit setup = let
         action = do
-            setOpener T.South
-            _ <- setup
+            setup `andNextBidderIs` T.South
         explanation =
             "We've found a trump fit and have slam interest. Time to check " .+
             "for keycards by bidding " .+ RKC.bRKC4N .+ "! If we're missing " .+
             "two of them, we'll sign off at the 5 level."
       in situation "init" action RKC.bRKC4N explanation
   in
-    wrapNW $ return sit <- setUpAuctions_
+    wrapNW $ return sit <~ setUpAuctions_
 
 
 topic1430 :: Topic
@@ -45,4 +70,3 @@ topic3014 = makeTopic "Roman Keycard Blackwood 3014" "RKC3014" situations
     situations = wrap [ initiate
                       , initiate
                       ]
-
