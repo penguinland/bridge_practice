0705ecfcdf30356e98703a57438e09cce66c6282
Alan Davidson <alan.davidson@gmail.com>
2025-11-29 21:33:38 -0500

    start adding support to predeal cards in the dealer program

--- src/DealerProg.hs
+++ src/DealerProg.hs
@@ -20,42 +20,51 @@ import qualified Terminology as T
 
 
 -- Contains all definitions and then required conditions
-data DealerProg = DealerProg DealerDefs [CondName]
+data DealerProg = DealerProg [Predeal] DealerDefs [CondName]
+
+
+-- Performance optimization: for rare auctions (e.g., slam investigations), we
+-- can greatly speed up how long it takes to generate a board by specifying
+-- exactly how long certain suits are in certain hands. Inequalities don't work
+-- here: they must be exactly that long.
+-- These get turned into dealer syntax like `predeal spades(north) == 1`
+type Predeal = (T.Suit, T.Direction, Int)
 
 
 instance Semigroup DealerProg where
-    (DealerProg defnsA reqsA) <> (DealerProg defnsB reqsB) =
-        DealerProg (defnsA <> defnsB) (reqsB ++ reqsA)
+    (DealerProg predealA defnsA reqsA) <> (DealerProg predealB defnsB reqsB) =
+        DealerProg (predealA ++ predealB) (defnsA <> defnsB) (reqsB ++ reqsA)
 
 
 instance Monoid DealerProg where
-    mempty = DealerProg mempty []
+    mempty = DealerProg [] mempty []
 
 
 addDefn :: CondName -> CondDefn -> DealerProg -> DealerProg
-addDefn name defn (DealerProg m l) =
-  DealerProg (addDefinition name defn m) l
+addDefn name defn (DealerProg p m l) =
+  DealerProg p (addDefinition name defn m) l
 
 
 addNewReq :: CondName -> CondDefn -> DealerProg -> DealerProg
-addNewReq name defn (DealerProg m l) =
-  DealerProg (addDefinition name defn m) (name:l)
+addNewReq name defn (DealerProg p m l) =
+  DealerProg p (addDefinition name defn m) (name:l)
 
 
 invert :: DealerProg -> DealerProg
-invert (DealerProg defns reqs) =
-    DealerProg defns ["!(" ++ join " && " reqs ++ ")"]
+invert (DealerProg predeals defns reqs) =
+    DealerProg predeals defns ["!(" ++ join " && " reqs ++ ")"]
 
 
 nameAll :: CondName -> DealerProg -> DealerProg
-nameAll name (DealerProg defns reqs) =
+nameAll name (DealerProg predeals defns reqs) =
     if (length reqs > 0)
-    then addNewReq name (join " && " . reverse $ reqs) (DealerProg defns mempty)
-    else DealerProg defns reqs  -- Unchanged
+    then addNewReq name (join " && " . reverse $ reqs)
+        (DealerProg predeals defns mempty)
+    else DealerProg predeals defns reqs  -- Unchanged
 
 
 toProgram :: DealerProg -> String
-toProgram (DealerProg defns conds) = unlines $
+toProgram (DealerProg predeals defns conds) = unlines $
     -- This used to generate 1 million boards, but some situations are extremely
     -- rare (e.g., North opens a Precision 1D with 5 hearts and South responds
     -- 2N: occurs less than twice in a million deals), and if you run the dealer
@@ -65,10 +74,14 @@ toProgram (DealerProg defns conds) = unlines $
     -- isn't enough, consider removing those situations from practice entirely,
     -- since they'll probably never come up.
     ["generate 10000000", "produce 1", ""] ++
+    map formatPredeal predeals ++
     toProgDefs defns
     ++ ["", "condition",
         "    " ++ (join " && " . reverse $ conds),
         "action", "    printall"]
+  where
+    formatPredeal (suit, dir, len) =
+        "predeal " ++ show suit ++ "(" ++ show dir ++ ") == " ++ show len
 
 
 eval :: String -> T.Direction -> T.Vulnerability -> DealerProg -> Int ->
