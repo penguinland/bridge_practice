627f059591a8d822dfb07b34447d689434ffa823
Alan Davidson <alan.davidson@gmail.com>
2025-12-02 01:05:41 -0500

    separate RNGs for each worker thread

--- server/ThreadPool.hs
+++ server/ThreadPool.hs
@@ -4,20 +4,25 @@ module ThreadPool(ThreadPool, newThreadPool, enqueue, StIO) where
 import Control.Concurrent(forkIO)
 import Control.Concurrent.Classy.BoundedChan(
     BoundedChan, newBoundedChan, writeBoundedChan, readBoundedChan)
-import Control.Monad(replicateM_)
 import Control.Monad.Trans.State.Strict(StateT, runStateT)
-import System.Random(StdGen)
+import System.Random(StdGen, split)
 
 
 type StIO = StateT StdGen IO
 type ThreadPool = BoundedChan IO (StIO ())
 
 
+splitRNG :: StdGen -> Int -> [StdGen]
+splitRNG _ 0 = []
+splitRNG rng n = let (rng', rng'') = split rng
+  in rng' : splitRNG rng'' (n - 1)
+
+
 newThreadPool :: Int -> StdGen -> IO ThreadPool
 newThreadPool nThreads rng = do
-    channel <- newBoundedChan nThreads
-    -- TODO: get separate RNGs for each worker
-    replicateM_ nThreads . forkIO $ runWorker channel rng
+    channel <- newBoundedChan (nThreads * 2)
+    let rngs = splitRNG rng nThreads
+    sequence_ . map (forkIO . runWorker channel) $ rngs
     return channel
   where
     runWorker :: ThreadPool -> StdGen -> IO ()
