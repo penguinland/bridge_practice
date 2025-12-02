15f63796ba9b5c94c050ac0ea4868d7eb0cccd4e
Alan Davidson <alan.davidson@gmail.com>
2025-12-02 00:48:26 -0500

    this is still not working

--- server/Cacher.hs
+++ server/Cacher.hs
@@ -1,8 +1,12 @@
-module Cacher(Cacher, newCacher, getProblem) where
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+--module Cacher(Cacher, newCacher, getProblem) where
+module Cacher(Cacher, newCacher) where
 
 import Control.Concurrent.MVar(MVar, newMVar, takeMVar, putMVar)
-import Control.Concurrent.Pool(Pool, newPool, queue)
-import Control.Monad(replicateM)
+import Control.Concurrent.Pool(Pool, newPool, queue, Task, runTask)
+import Control.Monad(replicateM_)
 import Control.Monad.Trans(liftIO)
 import Control.Monad.Trans.State.Strict(StateT)
 import System.Random(StdGen)
@@ -12,30 +16,38 @@ import Topic(Topic)
 import SituationInstance(SituationInstance)
 
 
-data Cacher = Cacher Topic (MVar [SituationInstance])
+type StIO = StateT StdGen IO
+type PoolType = Pool StIO () ()
+
+instance Task StIO a r where
+    runTask = liftIO runTask
 
 
-_threadPool :: Pool (StateT StdGen IO) Cacher a
-_threadPool = newPool 4 False  -- 4 threads, don't return results
+data Cacher = Cacher Topic (MVar [SituationInstance]) PoolType
 
 
-newCacher :: Topic -> Int -> StateT StdGen IO Cacher
-newCacher topic cacheCount = do
+makeThreadPool :: StIO PoolType
+makeThreadPool = newPool 4 False  -- 4 threads, don't return results
+
+
+newCacher :: PoolType -> Int -> Topic -> StIO Cacher
+newCacher pool cacheCount topic = do
     mv <- liftIO $ newMVar []
-    let cacher = Cacher topic mv
-    replicateM cacheCount $ queue _threadPool makeProblem_ cacher
+    let cacher = Cacher topic mv pool
+    --liftIO . replicateM_ cacheCount $ queue pool (makeProblem_ cacher) ()
+    queue pool (makeProblem_ cacher) ()
     return cacher
 
 
-makeProblem_ :: Cacher -> StateT StdGen IO ()
-makeProblem_ (Cacher t mv) = do
+makeProblem_ :: Cacher -> StIO ()
+makeProblem_ (Cacher t mv _) = do
     sitInst <- generate 1 [t]
     sitInsts <- liftIO $ takeMVar mv
     liftIO $ putMVar mv ((sitInst !! 0) : sitInsts)
 
-
-getProblem :: Cacher -> StateT StdGen IO SituationInstance
-getProblem c@(Cacher t mv) = do
+{-
+getProblem :: Cacher -> StIO SituationInstance
+getProblem c@(Cacher t mv p) = do
     sitInsts <- liftIO $ takeMVar mv
     case sitInsts of
       [] -> do
@@ -44,5 +56,6 @@ getProblem c@(Cacher t mv) = do
         return $ newSitInsts !! 0
       (first:rest) -> do
         liftIO $ putMVar mv rest
-        queue _threadPool makeProblem_ c
+        liftIO $ queue p (makeProblem_ c) ()
         return first
+-}
