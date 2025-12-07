4a2065729faf420058e2a941e07111501f881b2c
Alan Davidson <alan.davidson@gmail.com>
2025-12-06 22:38:59 -0500

    all queen ask responses are postalertable

--- src/Bids/RomanKeycardBlackwood.hs
+++ src/Bids/RomanKeycardBlackwood.hs
@@ -354,7 +354,8 @@ bH5C5D5S = E.nameAction "RKC_H_5C5D5S" $ do
     E.hasCard T.Hearts 'Q'
     E.hasCard T.Spades 'K'
     E.makeAlertableCall (T.Bid 5 T.Spades)
-                        ("have " .+ T.Hearts .+ "Q, " .+ T.Spades .+ "K")
+                        ("(postalert) have " .+ T.Hearts .+ "Q, " .+
+                         T.Spades .+ "K")
 
 bH5C5D6C :: Action
 bH5C5D6C = E.nameAction "RKC_H_5C5D6C" $ do
@@ -362,8 +363,8 @@ bH5C5D6C = E.nameAction "RKC_H_5C5D6C" $ do
     E.forbid $ E.hasCard T.Spades 'K'
     E.hasCard T.Clubs 'K'
     E.makeAlertableCall (T.Bid 6 T.Clubs)
-                        ("have " .+ T.Hearts .+ "Q, " .+ T.Clubs .+ "K, " .+
-                         "no " .+ T.Spades .+ "K")
+                        ("(postalert) have " .+ T.Hearts .+ "Q, " .+
+                         T.Clubs .+ "K, " .+ "no " .+ T.Spades .+ "K")
 
 bH5C5D6D :: Action
 bH5C5D6D = E.nameAction "RKC_H_5C5D6D" $ do
@@ -372,7 +373,8 @@ bH5C5D6D = E.nameAction "RKC_H_5C5D6D" $ do
     E.forbid $ E.hasCard T.Clubs 'K'
     E.hasCard T.Diamonds 'K'
     E.makeAlertableCall (T.Bid 6 T.Diamonds)
-                        ("have " .+ T.Hearts .+ "Q, " .+ T.Diamonds .+ "K, no black king")
+                        ("(postalert) have " .+ T.Hearts .+ "Q, " .+
+                         T.Diamonds .+ "K, no black king")
 
 bH5C5D6H :: Action
 bH5C5D6H = E.nameAction "RKC_H_5C5D6H" $ do
@@ -381,7 +383,7 @@ bH5C5D6H = E.nameAction "RKC_H_5C5D6H" $ do
     E.forbid $ E.hasCard T.Clubs 'K'
     E.forbid $ E.hasCard T.Diamonds 'K'
     E.makeAlertableCall (T.Bid 6 T.Hearts)
-                        ("have " .+ T.Hearts .+ "Q, no side king")
+                        ("(postalert) have " .+ T.Hearts .+ "Q, no side king")
 
 bH5D5S5N :: Action
 bH5D5S5N = E.nameAction "RKC_H_5D5S5N" $ do
@@ -389,14 +391,15 @@ bH5D5S5N = E.nameAction "RKC_H_5D5S5N" $ do
     E.forbid $ E.hasCard T.Clubs 'K'
     E.forbid $ E.hasCard T.Diamonds 'K'
     E.makeAlertableCall (T.Bid 5 T.Notrump)
-                        ("have " .+ T.Hearts .+ "Q, no minor king")
+                        ("(postalert) have " .+ T.Hearts .+ "Q, no minor king")
 
 bH5D5S6C :: Action
 bH5D5S6C = E.nameAction "RKC_H_5D5S6C" $ do
     E.hasCard T.Hearts 'Q'
     E.hasCard T.Clubs 'K'
     E.makeAlertableCall (T.Bid 6 T.Clubs)
-                        ("have " .+ T.Hearts .+ "Q, " .+ T.Clubs .+ "K")
+                        ("(postalert) have " .+ T.Hearts .+ "Q, " .+
+                         T.Clubs .+ "K")
 
 bH5D5S6D :: Action
 bH5D5S6D = E.nameAction "RKC_H_5D5S6D" $ do
@@ -404,8 +407,8 @@ bH5D5S6D = E.nameAction "RKC_H_5D5S6D" $ do
     E.forbid $ E.hasCard T.Clubs 'K'
     E.hasCard T.Diamonds 'K'
     E.makeAlertableCall (T.Bid 6 T.Diamonds)
-                        ("have " .+ T.Hearts .+ "Q, " .+ T.Diamonds .+ "K, " .+
-                         "no " .+ T.Clubs .+ "K")
+                        ("(postalert) have " .+ T.Hearts .+ "Q, " .+
+                         T.Diamonds .+ "K, " .+ "no " .+ T.Clubs .+ "K")
 
 
 bS5C5D5H :: Action
@@ -413,7 +416,8 @@ bS5C5D5H = E.nameAction "RKC_S_5C5D5H" $ do
     E.hasCard T.Spades 'Q'
     E.hasCard T.Hearts 'K'
     E.makeAlertableCall (T.Bid 5 T.Hearts)
-                        ("have " .+ T.Spades .+ "S, " .+ T.Hearts .+ "K")
+                        ("(postalert) have " .+ T.Spades .+ "S, " .+
+                         T.Hearts .+ "K")
 
 bS5C5D6C :: Action
 bS5C5D6C = E.nameAction "RKC_S_5C5D6C" $ do
@@ -421,8 +425,8 @@ bS5C5D6C = E.nameAction "RKC_S_5C5D6C" $ do
     E.forbid $ E.hasCard T.Hearts 'K'
     E.hasCard T.Clubs 'K'
     E.makeAlertableCall (T.Bid 6 T.Clubs)
-                        ("have " .+ T.Spades .+ "Q, " .+ T.Clubs .+ "K, " .+
-                         "no " .+ T.Hearts .+ "K")
+                        ("(postalert) have " .+ T.Spades .+ "Q, " .+
+                         T.Clubs .+ "K, " .+ "no " .+ T.Hearts .+ "K")
 
 bS5C5D6D :: Action
 bS5C5D6D = E.nameAction "RKC_S_5C5D6D" $ do
@@ -431,8 +435,8 @@ bS5C5D6D = E.nameAction "RKC_S_5C5D6D" $ do
     E.forbid $ E.hasCard T.Clubs 'K'
     E.hasCard T.Diamonds 'K'
     E.makeAlertableCall (T.Bid 6 T.Diamonds)
-                        ("have " .+ T.Spades .+ "Q, " .+ T.Diamonds .+ "K, " .+
-                         "no other side king")
+                        ("(postalert) have " .+ T.Spades .+ "Q, " .+
+                         T.Diamonds .+ "K, " .+ "no other side king")
 
 bS5C5D6S :: Action
 bS5C5D6S = E.nameAction "RKC_S_5C5D6S" $ do
@@ -441,7 +445,7 @@ bS5C5D6S = E.nameAction "RKC_S_5C5D6S" $ do
     E.forbid $ E.hasCard T.Clubs 'K'
     E.forbid $ E.hasCard T.Diamonds 'K'
     E.makeAlertableCall (T.Bid 6 T.Spades)
-                        ("have " .+ T.Spades .+ "Q, no side king")
+                        ("(postalert) have " .+ T.Spades .+ "Q, no side king")
 
 
 bS5D5H6C :: Action
@@ -449,7 +453,8 @@ bS5D5H6C = E.nameAction "RKC_S_5D5H6C" $ do
     E.hasCard T.Spades 'Q'
     E.hasCard T.Clubs 'K'
     E.makeAlertableCall (T.Bid 6 T.Clubs)
-                        ("have " .+ T.Spades .+ "Q, " .+ T.Clubs .+ "K")
+                        ("(postalert) have " .+ T.Spades .+ "Q, " .+
+                         T.Clubs .+ "K")
 
 bS5D5H6D :: Action
 bS5D5H6D = E.nameAction "RKC_S_5D5H6D" $ do
@@ -457,8 +462,8 @@ bS5D5H6D = E.nameAction "RKC_S_5D5H6D" $ do
     E.forbid $ E.hasCard T.Clubs 'K'
     E.hasCard T.Diamonds 'K'
     E.makeAlertableCall (T.Bid 6 T.Diamonds)
-                        ("have " .+ T.Spades .+ "Q, " .+ T.Diamonds .+ "K, " .+
-                         "no " .+ T.Clubs .+ "K")
+                        ("(postalert) have " .+ T.Spades .+ "Q, " .+
+                         T.Diamonds .+ "K, " .+ "no " .+ T.Clubs .+ "K")
 
 bS5D5H6H :: Action
 bS5D5H6H = E.nameAction "RKC_S_5D5H6H" $ do
@@ -467,8 +472,8 @@ bS5D5H6H = E.nameAction "RKC_S_5D5H6H" $ do
     E.forbid $ E.hasCard T.Diamonds 'K'
     E.hasCard T.Hearts 'K'
     E.makeAlertableCall (T.Bid 6 T.Hearts)
-                        ("have " .+ T.Spades .+ "S, " .+ T.Hearts .+ "K, " .+
-                         "no minor king")
+                        ("(postalert) have " .+ T.Spades .+ "S, " .+
+                         T.Hearts .+ "K, " .+ "no minor king")
 
 bS5D5H6S :: Action
 bS5D5H6S = E.nameAction "RKC_S_5D5H6S" $ do
@@ -477,4 +482,4 @@ bS5D5H6S = E.nameAction "RKC_S_5D5H6S" $ do
     E.forbid $ E.hasCard T.Diamonds 'K'
     E.forbid $ E.hasCard T.Hearts 'K'
     E.makeAlertableCall (T.Bid 6 T.Spades)
-                        ("have " .+ T.Spades .+ "Q, no side king")
+                        ("(postalert) have " .+ T.Spades .+ "Q, no side king")
