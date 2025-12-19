e95a2755e9334dcd6cd9e30c44e3892cd2aca077
Alan Davidson <alan.davidson@gmail.com>
2025-12-19 01:01:21 -0500

    bring back the compile time assertion that debug strings must be unique within a topic, doesn't pass yet

--- src/Assertions.hs
+++ src/Assertions.hs
@@ -3,12 +3,13 @@
 -- (e.g., topicList) must be already compiled in a separate file.
 module Assertions where
 
---import Control.Monad.Extra(mconcatMapM)
+import Control.Monad.Extra(mconcatMapM)
 import Data.Tuple.Utils(fst3, thd3)
 
+import Collection(survey)
 import CompileTime(staticAssert, duplicatesOf)
---import Output(toMonospace)
---import Situation(sitRef)
+import Output(toMonospace)
+import Situation(sitRef)
 import SupportedTopics(topicList)
 import Topic(Topic(..))
 
@@ -22,14 +23,7 @@ $(let dups = duplicatesOf . map (refName . thd3) $ topicList
     ("topics have duplicate debug names: " ++ show dups))
 
 
--- Asserting that every situation within a topic has a unique name sounds like
--- it ought to be a good idea. However, there are times when a topic contains a
--- `wrap [sitA, sitA, sitB]` to get situation A to be twice as common as
--- situation B, at which point the situation names are purposely _not_ unique!
--- Think about ways to improve this in the future, and maybe try again sometime.
-{-
 -- Something about $(...) seems to mess up indentation parsing, I think? If you
 -- try adding newlines where they logically belong, this becomes unparseable.
-$(let assertUniqueNames topic = (let dups = (duplicatesOf . collect sitRef $ topic) in staticAssert (null dups) ("duplicate situation names for topic " ++ (toMonospace . topicName $ topic) ++ ": " ++ show dups))
+$(let assertUniqueNames topic = (let dups = (duplicatesOf . survey sitRef . topicSituations $ topic) in staticAssert (null dups) ("duplicate situation names for topic " ++ (toMonospace . topicName $ topic) ++ ": " ++ show dups))
   in mconcatMapM (assertUniqueNames . thd3) $ topicList)
--}
