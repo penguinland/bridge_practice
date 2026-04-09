6985c38e39fe9d5ea5aee604407e51ff3afe9802
Alan Davidson <alan.davidson@gmail.com>
2026-04-09 08:08:46 -0400

    unexport CommonBids.takeoutDouble

--- src/CommonBids.hs
+++ src/CommonBids.hs
@@ -12,7 +12,6 @@ module CommonBids(
 , fourthSeatOpener
 , setOpener
 , andNextBidderIs
-, takeoutDouble
 , noInterference
 ) where
 
@@ -178,8 +177,8 @@ andNextBidderIs action direction = let
     action
 
 
-takeoutDouble :: T.Suit -> Action
-takeoutDouble shortSuit = nameAction ("toX_" ++ T.suitLetter shortSuit) $ do
+_takeoutDouble :: T.Suit -> Action
+_takeoutDouble shortSuit = nameAction ("toX_" ++ T.suitLetter shortSuit) $ do
     pointRange 11 40
     forEach T.allSuits setSuitLength
   where
@@ -196,7 +195,7 @@ noInterference suit = nameAction ("pass_over_" ++ T.suitLetter suit) $ do
     -- it here is too complicated for now. Let's just say the opponent has at
     -- most 14 HCP, and either doesn't have a 5-card suit or has at most 7 HCP.
     pointRange 0 14
-    forbid $ takeoutDouble suit
+    forbid $ _takeoutDouble suit
     alternatives [
         constrain "no_five_card_suit" [
             "shape(", ", any 4441, any 4333, any 4432)"]
