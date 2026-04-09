91d31131430b27abb7b91235efeb141d3bf04e29
BobChuckyJoe <33294760+BobChuckyJoe@users.noreply.github.com>
2026-04-09 07:48:34 -0400

    feat: Jordan 2NT (#224)
    * jordan 2nt bids
    
    * added topic
    
    * todos for jordan 2nt
    
    * Update src/Topics/Jordan2NT.hs
    
    Co-authored-by: Alan Davidson <alan@viam.com>
    
    * Update src/Topics/Jordan2NT.hs
    
    Co-authored-by: Alan Davidson <alan@viam.com>
    
    * Update src/Topics/Jordan2NT.hs
    
    Co-authored-by: Alan Davidson <alan@viam.com>
    
    * revert supported topics
    
    ---------
    
    Co-authored-by: Alan Davidson <alan@viam.com>

---
+++ src/Topics/Jordan2NT.hs
@@ -0,0 +1,54 @@
+module Topics.Jordan2NT(topic) where
+
+import qualified Bids.Jordan2NT as J 
+import qualified Bids.StandardOpenings as S 
+import qualified Bids.TakeoutDoubles as TD
+import CommonBids(setOpener)
+import Output((.+))
+import Situation(situation, (<~))
+import qualified Terminology as T
+import Topic(wrap, wrapDlr, Situations, Topic, makeTopic)
+
+
+jd2nt :: Situations
+jd2nt = let
+    sit (suit, setup, answer) = let
+        action = do
+            setOpener T.North
+            setup
+        explanation =
+            "We've got at least invitational strength with at least 4-card support " .+
+            "for partner's " .+ show suit .+ ". Bid Jordan " .+
+            T.Bid 2 T.Notrump .+ " to show this. Now that we've set trump, " .+
+            "partner can show their strength further."
+      in
+        situation "jd2n" action answer explanation
+  in
+    wrapDlr $ return sit <~ [
+        ( T.Hearts
+        , do setOpener T.North
+             S.b1H
+             TD.b1HoX
+        , J.b1HoX2N
+        )
+      , ( T.Spades
+        , do setOpener T.North
+             S.b1S
+             TD.b1SoX
+        , J.b1SoX2N
+        )
+    ]
+
+-- TODOs:
+--   - Add situation where partner has bid Jordan 2NT and as opener:
+--     - sign off with a minimum in 3M
+--     - help-suit game try with 4+ 
+--     - sign off with a maximum in 4M
+--   - Do the same thing for 1m opening
+--   - Have a situation where we would like to respond 3M with a weak hand
+--   - Likewise, we should pass 1M-X-3M as the opener knowing that our partner has a weak hand
+
+topic :: Topic
+topic = makeTopic ("Jordan " .+ T.Bid 2 T.Notrump)  "Jd2NT" $
+    wrap [ jd2nt
+         ]
